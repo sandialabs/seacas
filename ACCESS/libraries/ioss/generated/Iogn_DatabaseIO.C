13ce8e44abbcb413a98585bb59fa709a4ef05fbc
Greg Sjaardema <gdsjaar@sandia.gov>
2012-05-21 15:58:32 -0600

    IOSS: Compression options, 64-bit int cleanups; compilation warning cleanups

--- ACCESS/libraries/ioss/generated/Iogn_DatabaseIO.C
+++ ACCESS/libraries/ioss/generated/Iogn_DatabaseIO.C
@@ -53,14 +53,16 @@ namespace Iogn {
 
   Ioss::DatabaseIO* IOFactory::make_IO(const std::string& filename,
 				       Ioss::DatabaseUsage db_usage,
-				       MPI_Comm communicator) const
-  { return new DatabaseIO(NULL, filename, db_usage, communicator); }
+				       MPI_Comm communicator,
+				       const Ioss::PropertyManager &properties) const
+  { return new DatabaseIO(NULL, filename, db_usage, communicator, properties); }
 
   // ========================================================================
   DatabaseIO::DatabaseIO(Ioss::Region *region, const std::string& filename,
 			 Ioss::DatabaseUsage db_usage,
-			 MPI_Comm communicator) :
-    Ioss::DatabaseIO(region, filename, db_usage, communicator), 
+			 MPI_Comm communicator,
+			 const Ioss::PropertyManager &properties) :
+    Ioss::DatabaseIO(region, filename, db_usage, communicator, properties), 
     m_generatedMesh(NULL),  spatialDimension(3), nodeCount(0),
     elementCount(0), nodeBlockCount(0),
     elementBlockCount(0), nodesetCount(0), sidesetCount(0),
@@ -448,6 +450,8 @@ namespace Iogn {
 	  IOSS_ERROR(errmsg);
 	}
 
+      } else if (field.get_name() == "ids") {
+	// Do nothing, just handles an idiosyncracy of the GroupingEntity
       } else {
 	num_to_get = Ioss::Utils::field_warning(cs, field, "input");
       }
