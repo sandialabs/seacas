7eb961f6ca8283cff039def79107a589905a03d8
Greg Sjaardema <gdsjaar@sandia.gov>
2013-03-28 08:37:51 -0600

    TPL: Add parallel library

---
+++ TPL/parallel/parallel-20110122/config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "parallel"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "bug-parallel@gnu.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "parallel"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "parallel 20110122"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "parallel"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "20110122"
+
+/* Version number of package */
+#define VERSION "20110122"
