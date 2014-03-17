8ce39975f557facea264cfa25c4a4def8867585c
Greg Sjaardema <gsjaardema@gmail.com>
2014-03-17 15:18:14 -0600

    APREPRO: Array capability, comment string, sync with internal

--- ACCESS/libraries/aprepro/aprepro.h
+++ ACCESS/libraries/aprepro/aprepro.h
@@ -19,6 +19,17 @@
  * SEAMS::Parser, SEAMS::Scanner and SEAMS::Aprepro */
 namespace SEAMS {
 
+  /* Array structure */
+  struct array {
+    std::vector<double> data;
+    int rows;
+    int cols;
+    
+    array(int r, int c) : rows(r), cols(c) {data.resize(r*c);}
+    array() : rows(0), cols(0) {}
+    ~array() {}
+  };
+
   struct symrec
   {
     std::string name;
@@ -38,13 +49,20 @@ namespace SEAMS {
       double (*fnctptr_ddd)(double,double,double);
       double (*fnctptr_dddd)(double,double,double,double);
       double (*fnctptr_dddddd)(double,double,double,double,double,double);
+      double (*fnctptr_a)(const array*);
       const char *svar;
       const char *(*strfnct)();
       const char *(*strfnct_c)(char*);
       const char *(*strfnct_d)(double);
+      const char *(*strfnct_a)(const array*);
       const char *(*strfnct_ccc)(char*,char*,char*);
       const char *(*strfnct_dcc)(double,char*,char*);
       const char *(*strfnct_dcccc)(double, char*, char*, char*, char*);
+      array *avar; /* Array Variable */
+      array *(*arrfnct_c)(const char*);
+      array *(*arrfnct_dd)(double,double);
+      array *(*arrfnct_d)(double);
+      array *(*arrfnct_a)(const array*);
     } value;
     symrec *next;
 
@@ -68,6 +86,7 @@ namespace SEAMS {
   {
     std::string include_path;
     std::string include_file;
+    std::string comment;
     bool end_on_exit;
     bool warning_msg;
     bool info_msg;
@@ -77,6 +96,7 @@ namespace SEAMS {
     bool trace_parsing;    // enable debug output in the bison parser
 
     aprepro_options() :
+      comment("#"),
       end_on_exit(false),
       warning_msg(true),
       info_msg(false),
@@ -119,6 +139,8 @@ namespace SEAMS {
       UNDEFINED_VARIABLE = 5,
       FUNCTION = 3,
       STRING_FUNCTION = 4,
+      ARRAY_FUNCTION = 6,
+      ARRAY_VARIABLE = 7,
       IMMUTABLE_VARIABLE = 11,
       IMMUTABLE_STRING_VARIABLE = 12
     };
@@ -185,7 +207,7 @@ namespace SEAMS {
     void dumpsym (int type, bool doInternal) const;
   private:
     
-    void init_table(char comment);
+    void init_table(const char *comment);
     std::vector<symrec*> sym_table;
     std::ostringstream parsingResults;
   public:
