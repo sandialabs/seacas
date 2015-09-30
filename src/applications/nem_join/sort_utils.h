b04095d53c0087ed90776f496e741bfb00fd1799
Greg Sjaardema <gsjaardema@gmail.com>
2015-09-30 08:18:35 -0600

    NEM_JOIN: Remove, should not be used anymore since epu has all needed capabilities

--- src/applications/nem_join/sort_utils.h
+++
@@ -1,4 +0,0 @@
-extern void gds_iqsort(int v[], int iv[], int N);
-extern void gds_qsort(int v[], int N);
-extern int  gds_ibin_search(int value, int v[], int iv[], int N);
-extern int  gds_bin_search(int  value, int v[], int N);
