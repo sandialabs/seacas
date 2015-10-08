60fe94232542013247f8cb4166ced10f673fef9a
Greg Sjaardema <gsjaardema@gmail.com>
2015-10-08 10:45:56 -0400

    Hopefully improved finding of matio hdf5 dependency

--- cmake/TPLs/FindTPLMatio.cmake
+++ cmake/TPLs/FindTPLMatio.cmake
@@ -53,7 +53,7 @@
 # ************************************************************************
 # @HEADER
 
-
+find_package(Matio REQUIRED)
 TRIBITS_TPL_FIND_INCLUDE_DIRS_AND_LIBRARIES( Matio
   REQUIRED_HEADERS matio.h
   REQUIRED_LIBS_NAMES "matio")
