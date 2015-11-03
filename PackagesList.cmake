073186093049558b66322e207c145795958118d3
Greg Sjaardema <gdsjaar@sandia.gov>
2015-11-03 09:42:11 -0700

    Update to use a more-trilinos-like structure

--- PackagesList.cmake
+++ PackagesList.cmake
@@ -1,4 +1,12 @@
+#
+# Define the SEACASProj packages
+#
 TRIBITS_REPOSITORY_DEFINE_PACKAGES(
-	Zoltan		packages/zoltan/zoltan-3.82          PT
-	SEACAS		packages/seacas			     PT
-)
+  Zoltan                packages/zoltan                   PT
+  SEACAS                packages/seacas                   PT # Depends on netcdf, optionally hdf5, xdmf, pamgen
+  )
+
+# Allow builds even if some packages are missing
+
+TRIBITS_DISABLE_PACKAGE_ON_PLATFORMS(SEACAS Windows)
+TRIBITS_DISABLE_PACKAGE_ON_PLATFORMS(Zoltan Windows)
