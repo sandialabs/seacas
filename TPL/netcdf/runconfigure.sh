229199855f8687b3800711cef0d82b8cff0e9249
Greg Sjaardema <gsjaardema@gmail.com>
2015-11-12 08:42:03 -0500

    TPL: netcdf configure script now uses cmake instead of configure

--- TPL/netcdf/runconfigure.sh
+++ TPL/netcdf/runconfigure.sh
@@ -1,18 +1,34 @@
 #! /bin/sh
-rm -f config.cache
-CC='gcc'; export CC
-CFLAGS="-I${ACCESS}/include"; export CFLAGS
-CPPFLAGS="-DNDEBUG"; export CPPFLAGS
 
-# Find the hdf5 library
-LDFLAGS="-L${ACCESS}/lib"; export LDFLAGS
+### Set to ON for parallel compile; otherwise OFF for serial (default)
+MPI=OFF
+SHARED="ON"
 
-PNETCDF=""
-PARALLEL_TESTS=""
-# Only if parallel...
-#PNETCDF="--enable-pnetcdf"
-#PARALLEL_TESTS="--enable-parallel-tests"
+### Change this to point to the compilers you want to use
+if [ "$MPI" == "ON" ]
+then
+  CXX=mpicxx
+  CC=mpicc
+  FC=mpif77
+  PNETCDF="ON"
+else
+  CXX=g++
+  CC=gcc
+  FC=gfortran
+  PNETCDF="OFF"
+fi
 
-#SHARED="--enable-shared"
 
-./configure --enable-netcdf-4 ${PNETCDF} ${SHARED} ${PARALLEL_TESTS} --disable-fsync --prefix ${ACCESS} --disable-dap --disable-cdmremote $1
+rm -f CMakeCache.txt
+
+cmake .. -DCMAKE_C_COMPILER:FILEPATH=${CC} \
+         -DHDF5_DIR=${ACCESS} \
+         -DENABLE_PNETCDF=${PNETCDF} \
+         -DENABLE_PARALLEL4=${MPI} \
+         -DENABLE_PARALLEL_TESTS=${MPI} \
+         -DENABLE_NETCDF_4=ON \
+         -DENABLE_DAP=OFF \
+         -DENABLE_FSYNC=OFF \
+         -DENABLE_V2_API=OFF \
+         -DBUILD_SHARED_LIBS=${SHARED} \
+         -DCMAKE_INSTALL_PREFIX=${ACCESS}
