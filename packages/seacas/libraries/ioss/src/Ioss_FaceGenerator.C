cc9915252441c5901c2001b58695acf71a345a37
Greg Sjaardema <gsjaardema@gmail.com>
2016-02-29 10:35:50 -0700

    Sync with some changes on the sierra side

--- packages/seacas/libraries/ioss/src/Ioss_FaceGenerator.C
+++ packages/seacas/libraries/ioss/src/Ioss_FaceGenerator.C
@@ -30,6 +30,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#include <Ioss_CodeTypes.h>
 #include <Ioss_FaceGenerator.h>
 #include "Ioss_CommSet.h"
 #include "Ioss_DBUsage.h"
