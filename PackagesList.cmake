cd92cc717f4dafe13f513f2a0ac7859264c7df09
Greg Sjaardema <gsjaardema@gmail.com>
2016-02-28 21:12:04 -0700

    Clean up some more non-exodus stuff

--- PackagesList.cmake
+++ PackagesList.cmake
@@ -2,11 +2,9 @@
 # Define the SEACASProj packages
 #
 TRIBITS_REPOSITORY_DEFINE_PACKAGES(
-  Zoltan                packages/zoltan                   PT
   SEACAS                packages/seacas                   PT # Depends on netcdf, optionally hdf5, xdmf, pamgen
   )
 
 # Allow builds even if some packages are missing
 
 TRIBITS_DISABLE_PACKAGE_ON_PLATFORMS(SEACAS Windows)
-TRIBITS_DISABLE_PACKAGE_ON_PLATFORMS(Zoltan Windows)
