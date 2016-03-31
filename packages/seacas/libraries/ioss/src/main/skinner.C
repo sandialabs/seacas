892d1bd2e5bedddaa8be70948f9c1639adbfa724
Greg Sjaardema <gsjaardema@gmail.com>
2016-03-31 07:27:17 -0600

    IOSS: Clean up whitespace following clang-tidy

--- packages/seacas/libraries/ioss/src/main/skinner.C
+++ packages/seacas/libraries/ioss/src/main/skinner.C
@@ -2,14 +2,14 @@
 // Sandia Corporation. Under the terms of Contract
 // DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
 // certain rights in this software.
-//         
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
-// 
+//
 //     * Redistributions of source code must retain the above copyright
 //       notice, this list of conditions and the following disclaimer.
-// 
+//
 //     * Redistributions in binary form must reproduce the above
 //       copyright notice, this list of conditions and the following
 //       disclaimer in the documentation and/or other materials provided
@@ -17,7 +17,7 @@
 //     * Neither the name of Sandia Corporation nor the names of its
 //       contributors may be used to endorse or promote products derived
 //       from this software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -70,7 +70,7 @@
 // ========================================================================
 
 namespace {
-  template <typename INT> 
+  template <typename INT>
   void skinner(Skinner::Interface& interface, INT /*dummy*/);
   std::string codename;
   std::string version = "0.6";
@@ -83,17 +83,17 @@ int main(int argc, char *argv[])
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 #endif
-  
+
   Skinner::Interface interface;
   interface.parse_options(argc, argv);
-  
+
   std::string in_type = "exodusII";
 
   codename = argv[0];
   size_t ind = codename.find_last_of('/', codename.size());
   if (ind != std::string::npos) {
     codename = codename.substr(ind+1, codename.size());
-}
+  }
 
   Ioss::Init::Initializer io;
 
@@ -127,7 +127,7 @@ namespace {
   {
     std::string inpfile = interface.input_filename();
     std::string input_type = interface.input_type();
-    
+
     Ioss::PropertyManager properties;
     if (interface.ints_64_bit()) {
       properties.add(Ioss::Property("INTEGER_SIZE_DB",  8));
@@ -136,7 +136,7 @@ namespace {
 
     if (interface.debug) {
       properties.add(Ioss::Property("LOGGING", 1));
-}
+    }
 
     if (!interface.decomp_method.empty()) {
       properties.add(Ioss::Property("DECOMPOSITION_METHOD", interface.decomp_method));
@@ -153,8 +153,8 @@ namespace {
 
     if (interface.ints_64_bit()) {
       dbi->set_int_byte_size_api(Ioss::USE_INT64_API);
-}
-    
+    }
+
     // NOTE: 'region' owns 'db' pointer at this time...
     Ioss::Region region(dbi, "region_1");
 
@@ -180,16 +180,16 @@ namespace {
 
     for (auto& face : faces) {
       if (face.elementCount_ == 2) {
-	interior++;
-	if (face.sharedWithProc_ != -1) {
-	  pboundary++;
-}
+        interior++;
+        if (face.sharedWithProc_ != -1) {
+          pboundary++;
+        }
       }
       else if (face.elementCount_ == 1) {
-	boundary++;
+        boundary++;
       } else {
-	error++;
-}
+        error++;
+      }
     }
 
 #ifdef HAVE_MPI
@@ -210,12 +210,12 @@ namespace {
              << "\tBoundary = " << boundary
              << "\tShared   = " << pboundary
              << "\tError = " << error << "\n"
-	     << "Total Time = " << std::chrono::duration<double, std::milli>(duration).count() << " ms\t"
-	     << (interior+boundary-pboundary/2)/std::chrono::duration<double>(duration).count()
-	     << " faces/second\n\n";
+             << "Total Time = " << std::chrono::duration<double, std::milli>(duration).count() << " ms\t"
+             << (interior+boundary-pboundary/2)/std::chrono::duration<double>(duration).count()
+             << " faces/second\n\n";
 
       OUTPUT << "Hash Statistics: Bucket Count = " << faces.bucket_count()
-	     << "\tLoad Factor = " << faces.load_factor() << "\n";
+             << "\tLoad Factor = " << faces.load_factor() << "\n";
       size_t numel = region.get_property("element_count").get_int();
       OUTPUT << "Faces/Element ratio = " << static_cast<double>(faces.size()) / numel << "\n";
     }
@@ -223,13 +223,13 @@ namespace {
     if (interface.no_output()) {
       return;
     }
-      
+
     // Get vector of all boundary faces which will be output as the skin...
     std::vector<Ioss::Face> boundary_faces;
     boundary_faces.reserve(boundary);
     for (auto& face : faces) {
       if (face.elementCount_ == 1) {
-	boundary_faces.push_back(face);
+        boundary_faces.push_back(face);
       }
     }
 
@@ -241,17 +241,17 @@ namespace {
     for (const auto& face : boundary_faces) {
       size_t face_node_count = 0;
       for (auto &gnode : face.connectivity_) {
-	if (gnode > 0) {
-	  face_node_count++;
-	  auto node = region.get_database()->node_global_to_local(gnode, true) - 1;
-	  ref_nodes[node] = 1;
-	}
+        if (gnode > 0) {
+          face_node_count++;
+          auto node = region.get_database()->node_global_to_local(gnode, true) - 1;
+          ref_nodes[node] = 1;
+        }
       }
       if (face_node_count == 3) {
-	tri++;
+        tri++;
       } else if (face_node_count == 4) {
-	quad++;
-}
+        quad++;
+      }
     }
 
     size_t ref_count = std::accumulate(ref_nodes.begin(), ref_nodes.end(), 0);
@@ -261,47 +261,47 @@ namespace {
     std::vector<double> coord_in;
     Ioss::NodeBlock *nb = region.get_node_blocks()[0];
     nb->get_field_data("mesh_model_coordinates", coord_in);
-    
+
     std::vector<INT>  ids;
     nb->get_field_data("ids", ids);
 
     std::vector<INT> ref_ids(ref_count);
     std::vector<double> coord_out(3*ref_count);
-    
+
     size_t j = 0;
     for (size_t i=0; i < ref_nodes.size(); i++) {
       if (ref_nodes[i] == 1) {
-	coord_out[3*j+0] = coord_in[3*i+0];
-	coord_out[3*j+1] = coord_in[3*i+1];
-	coord_out[3*j+2] = coord_in[3*i+2];
-	ref_ids[j++] = ids[i];
+        coord_out[3*j+0] = coord_in[3*i+0];
+        coord_out[3*j+1] = coord_in[3*i+1];
+        coord_out[3*j+2] = coord_in[3*i+2];
+        ref_ids[j++] = ids[i];
       }
     }
-    
+
     // Create output file...
     if (interface.compression_level > 0 || interface.shuffle) {
       properties.add(Ioss::Property("FILE_TYPE", "netcdf4"));
       properties.add(Ioss::Property("COMPRESSION_LEVEL", interface.compression_level));
       properties.add(Ioss::Property("COMPRESSION_SHUFFLE", interface.shuffle));
     }
-      
+
     if (interface.compose_output != "none") {
       properties.add(Ioss::Property("COMPOSE_RESULTS", "YES"));
       properties.add(Ioss::Property("COMPOSE_RESTART", "YES"));
       if (interface.compose_output != "default") {
-	properties.add(Ioss::Property("PARALLEL_IO_MODE", interface.compose_output));
-}
+        properties.add(Ioss::Property("PARALLEL_IO_MODE", interface.compose_output));
+      }
     }
 
     if (interface.netcdf4) {
       properties.add(Ioss::Property("FILE_TYPE", "netcdf4"));
     }
-    
+
     std::string file = interface.output_filename();
     std::string type = interface.output_type();
     Ioss::DatabaseIO *dbo = Ioss::IOFactory::create(type, file,
-						    Ioss::WRITE_RESTART, (MPI_Comm)MPI_COMM_WORLD,
-						    properties);
+                                                    Ioss::WRITE_RESTART, (MPI_Comm)MPI_COMM_WORLD,
+                                                    properties);
     if (dbo == nullptr || !dbo->ok(true)) {
       std::exit(EXIT_FAILURE);
     }
@@ -313,25 +313,25 @@ namespace {
 
     Ioss::NodeBlock *nbo = new Ioss::NodeBlock(output_region.get_database(), "nodeblock_1", ref_count, 3);
     output_region.add(nbo);
-    
+
     Ioss::ElementBlock *quadblock = nullptr;
     Ioss::ElementBlock *triblock = nullptr;
     if (quad > 0) {
       quadblock = new Ioss::ElementBlock(output_region.get_database(), "quad", "shell", quad);
       output_region.add(quadblock);
     }
-      
+
     if (tri > 0) {
       triblock = new Ioss::ElementBlock(output_region.get_database(), "tri", "trishell", tri);
       output_region.add(triblock);
     }
-      
+
     output_region.end_mode(Ioss::STATE_DEFINE_MODEL);
-      
+
     output_region.begin_mode(Ioss::STATE_MODEL);
     nbo->put_field_data("ids", ref_ids);
     nbo->put_field_data("mesh_model_coordinates", coord_out);
-    
+
     std::vector<INT> quad_conn;
     std::vector<INT> quad_ids;
     std::vector<INT> tri_conn;
@@ -345,24 +345,24 @@ namespace {
     INT fid = 1;
     for (auto& face : boundary_faces) {
       if (use_face_ids) {
-	fid = face.id_;
-	if (fid < 0) { fid = -fid;
-}
+        fid = face.id_;
+        if (fid < 0) { fid = -fid;
+        }
       } else {
-	fid++;
+        fid++;
       }
 
       if (face.connectivity_[3] != 0) {
-	for (int i=0; i < 4; i++) {
-	  quad_conn.push_back(face.connectivity_[i]);
-	}
-	quad_ids.push_back(fid);
+        for (int i=0; i < 4; i++) {
+          quad_conn.push_back(face.connectivity_[i]);
+        }
+        quad_ids.push_back(fid);
       }
       else {
-	for (int i=0; i < 3; i++) {
-	  tri_conn.push_back(face.connectivity_[i]);
-	}
-	tri_ids.push_back(fid);
+        for (int i=0; i < 3; i++) {
+          tri_conn.push_back(face.connectivity_[i]);
+        }
+        tri_ids.push_back(fid);
       }
     }
 
