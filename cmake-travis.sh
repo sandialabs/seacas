e167f3daa90deb44e815d52a87a47bb70b4f632d
Greg Sjaardema <gsjaardema@gmail.com>
2016-05-18 16:01:20 -0600

    Now try building / testing with mpich

--- cmake-travis.sh
+++ cmake-travis.sh
@@ -1,7 +1,15 @@
 #! /usr/bin/env sh
 
+### Change this to point to the compilers you want to use
+if [ "$MPI" == "ON" ]
+then
+  COMPILER = "-DCMAKE_CXX_COMPILER:FILEPATH=mpicxx -DCMAKE_C_COMPILER:FILEPATH=mpicc -DCMAKE_Fortran_COMPILER:FILEPATH=mpif77"
+else
+fi
+
 cmake \
-  -DBUILD_SHARED_LIBS:BOOL=ON \
+  ${COMPILER} -DBUILD_SHARED_LIBS:BOOL=ON \
+  -DTPL_ENABLE_MPI=${MPI} \
   -DCMAKE_CXX_FLAGS="-Wall -pedantic" \
   -DCMAKE_C_FLAGS="-Wall -pedantic" \
   -DHDF5_ROOT:PATH=/usr/ \
