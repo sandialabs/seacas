5640213ef83a6c5a8f112435aecef42faaa64c21
Greg Sjaardema <gsjaardema@gmail.com>
2016-05-19 06:59:13 -0600

    Making progress on mpi build

--- cmake-travis.sh
+++ cmake-travis.sh
@@ -4,12 +4,11 @@ MPI=$@
 ### Change this to point to the compilers you want to use
 if [ "$MPI" == "ON" ]
 then
-  COMPILER = "-DCMAKE_CXX_COMPILER:FILEPATH=mpicxx -DCMAKE_C_COMPILER:FILEPATH=mpicc -DCMAKE_Fortran_COMPILER:FILEPATH=mpif77"
+  COMPILER="-DTPL_ENABLE_MPI=ON -DCMAKE_CXX_COMPILER:FILEPATH=mpicxx -DCMAKE_C_COMPILER:FILEPATH=mpicc -DCMAKE_Fortran_COMPILER:FILEPATH=mpif77"
 fi
 
 cmake \
   ${COMPILER} -DBUILD_SHARED_LIBS:BOOL=ON \
-  -DTPL_ENABLE_MPI=${MPI} \
   -DCMAKE_CXX_FLAGS="-Wall -pedantic" \
   -DCMAKE_C_FLAGS="-Wall -pedantic" \
   -DHDF5_ROOT:PATH=/usr/ \
