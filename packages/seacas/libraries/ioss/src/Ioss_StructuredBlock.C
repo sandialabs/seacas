ce235c5c9facd387f0ab625dbe40e9dc89c30148
Greg Sjaardema <gsjaardema@gmail.com>
2016-05-27 11:17:46 -0600

    Initial cgns structured block support

--- packages/seacas/libraries/ioss/src/Ioss_StructuredBlock.C
+++ packages/seacas/libraries/ioss/src/Ioss_StructuredBlock.C
@@ -30,29 +30,51 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef IOSS_Ioss_EntityType_H
-#define IOSS_Ioss_EntityType_H
+#include "Ioss_FieldManager.h" // for FieldManager
+#include <Ioss_DatabaseIO.h>   // for DatabaseIO
+#include <Ioss_StructuredBlock.h>
+#include <Ioss_Field.h>    // for Field, etc
+#include <Ioss_Property.h> // for Property
+#include <stddef.h>        // for size_t
+#include <string>          // for string
+#include <vector>          // for vector
 
 namespace Ioss {
-  /** \brief The particular type of GroupingEntity.
+  class Field;
+
+  /** \brief Create a structured block.
+   *
+   *  \param[in] io_database The database associated with the region containing the structured block.
+   *  \param[in] my_name The structured block's name.
+   *  \param[in] ni The number of intervals in the (i) direction.
+   *  \param[in] nj The number of intervals in the (j) direction. Zero if 1D
+   *  \param[in] nk The number of intervals in the (k) direction. Zero if 2D
    */
-  enum EntityType {
-    NODEBLOCK    = 1,
-    EDGEBLOCK    = 2,
-    FACEBLOCK    = 4,
-    ELEMENTBLOCK = 8,
-    NODESET      = 16,
-    EDGESET      = 32,
-    FACESET      = 64,
-    ELEMENTSET   = 128,
-    SIDESET      = 256,
-    SURFACE      = 256, //: Same as sideset
-    COMMSET      = 512,
-    SIDEBLOCK    = 1024,
-    REGION       = 2048,
-    SUPERELEMENT = 4096,
-    STRUCTUREDBLOCK = 8192,
-    INVALID_TYPE = 16384
-  };
-}
-#endif
+  StructuredBlock::StructuredBlock(DatabaseIO *io_database, const std::string &my_name,
+				   int ni, int nj, int nk)
+      : GroupingEntity(io_database, my_name, 
+		       ni * (nj > 0 ? nj : 1) * (nk > 0 ? nk : 1)),
+	m_ni(ni), m_nj(nj), m_nk(nk)
+  {
+  }
+
+  StructuredBlock::~StructuredBlock() = default;
+
+  Property StructuredBlock::get_implicit_property(const std::string &my_name) const
+  {
+    return GroupingEntity::get_implicit_property(my_name);
+  }
+
+  int64_t StructuredBlock::internal_get_field_data(const Field &field, void *data,
+                                                size_t data_size) const
+  {
+    return get_database()->get_field(this, field, data, data_size);
+  }
+
+  int64_t StructuredBlock::internal_put_field_data(const Field &field, void *data,
+                                                size_t data_size) const
+  {
+    return get_database()->put_field(this, field, data, data_size);
+  }
+
+} // namespace Ioss
