d3fdc59d240af6383d6dda7d70bdcc9fde3327c1
Greg Sjaardema <gsjaardema@gmail.com>
2017-05-24 12:54:51 -0600

    Fix setting of ACCESS variable

--- TPL/cgns/runconfigure.sh
+++ TPL/cgns/runconfigure.sh
@@ -8,7 +8,7 @@ echo "MPI set to ${MPI}"
 
 ### The following assumes you are building in a subdirectory of ACCESS Root
 if [ "X$ACCESS" == "X" ] ; then
-  ACCESS=$(cd ../../..; pwd)
+  ACCESS=$(cd ../../../..; pwd)
   echo "ACCESS set to ${ACCESS}"
 fi
 
