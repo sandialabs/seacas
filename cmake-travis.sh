a6448e513bf3301696efa5adf33e8c009cf90263
Greg Sjaardema <gsjaardema@gmail.com>
2017-07-21 11:55:23 -0600

    CONFIG: Use CGNS master now that 3.3.1 is released

--- cmake-travis.sh
+++ cmake-travis.sh
@@ -33,7 +33,7 @@ fi
 cd TPL/cgns
 git clone https://github.com/cgns/CGNS
 cd CGNS
-git checkout develop
+# git checkout develop
 mkdir build
 cd build
 MPI=${MPI} ACCESS="/usr/local" bash ../../runconfigure.sh
