bc22108a1ed755f57b7b20a8a4f8091faa40f219
Greg Sjaardema <gsjaardema@gmail.com>
2017-07-28 14:25:46 -0600

    IOSS: fix compiler error

--- packages/seacas/libraries/ioss/src/exodus/Ioex_Internals.C
+++ packages/seacas/libraries/ioss/src/exodus/Ioex_Internals.C
@@ -3425,7 +3425,7 @@ namespace {
     int id_type = get_type(exoid, EX_IDS_INT64_API);
 
     if (id_type == NC_INT64) {
-      status = nc_put_var_longlong(exoid, var_id, const_cast<long long int *>(&ids[0]));
+      status = nc_put_var_longlong(exoid, var_id, (long long int *)&ids[0]);
     }
     else {
       // Have entity_id (long long), need ints...
