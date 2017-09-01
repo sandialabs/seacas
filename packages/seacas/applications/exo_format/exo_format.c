faf7b3b73d9162242ae0c4d8aa5964b806b2a2ab
Greg Sjaardema <gsjaardema@gmail.com>
2017-09-01 10:34:31 -0600

    EXO_FORMAT: Support older netcdf without CDF5 or 64BIT_OFFSET

--- packages/seacas/applications/exo_format/exo_format.c
+++ packages/seacas/applications/exo_format/exo_format.c
@@ -164,12 +164,14 @@ int main(int argc, char *argv[])
   if (nc_format == NC_FORMAT_CLASSIC) {
     fprintf(stderr, "\t\tNetCDF Variant is 'classic'\n");
   }
-  else if (nc_format == NC_FORMAT_64BIT_OFFSET) {
+  else if (nc_format == NC_FORMAT_64BIT) {
     fprintf(stderr, "\t\tNetCDF Variant is '64-bit offset'\n");
   }
+#if defined NC_FORMAT_64BIT_DATA
   else if (nc_format == NC_FORMAT_64BIT_DATA) {
     fprintf(stderr, "\t\tNetCDF Variant is '64-bit data (CDF5)'\n");
   }
+#endif
   else if (nc_format == NC_FORMAT_NETCDF4) {
     fprintf(stderr, "\t\tNetCDF Variant is 'netCDF-4'\n");
   }
