a0eb5deb7453cdb59c26354ffde8b36fbd9c822a
Greg Sjaardema <gsjaardema@gmail.com>
2017-09-27 08:56:31 -0600

    EXODUS: optimization for files with lots of timesteps

--- packages/seacas/libraries/exodus/include/exodusII.h
+++ packages/seacas/libraries/exodus/include/exodusII.h
@@ -68,8 +68,8 @@
 #endif
 
 /* EXODUS version number */
-#define EX_API_VERS 7.03f
-#define EX_API_VERS_NODOT 703
+#define EX_API_VERS 7.04f
+#define EX_API_VERS_NODOT 704
 #define EX_VERS EX_API_VERS
 #define NEMESIS_API_VERSION EX_API_VERS
 #define NEMESIS_API_VERSION_NODOT EX_API_VERS_NODOT
