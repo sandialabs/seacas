a3ebb391545f43de371e3c1d2c20bb73f7f877a1
Greg Sjaardema <gsjaardema@gmail.com>
2017-11-13 10:36:46 -0700

    Fixing travis build...

--- cgns-travis.sh
+++ cgns-travis.sh
@@ -4,5 +4,5 @@ cd CGNS
 # git checkout develop
 mkdir build
 cd build
-MPI=${MPI} ACCESS="/usr/local" bash ../../runconfigure.sh
+MPI=${MPI} ACCESS="/usr/local" bash ../../runconfigure.sh HDF5_ROOT=/usr/lib/x86_64-linux-gnu/
 make && sudo make install
