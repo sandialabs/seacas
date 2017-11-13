1284cf9a71f4cef9d909e58e9067f2a477f29ac6
Greg Sjaardema <gsjaardema@gmail.com>
2017-11-13 12:15:18 -0700

    Another try at new build

--- install-tpl.sh
+++ install-tpl.sh
@@ -8,14 +8,14 @@ pwd
 
 # =================== INSTALL HDF5 ===============
 hdf_version="1.8.19"
+
+cd $ACCESS
 cd TPL/hdf5
 wget https://support.hdfgroup.org/ftp/HDF5/current18/src/hdf5-${hdf_version}.tar.bz2
 tar -jxf hdf5-${hdf_version}.tar.bz2
 cd hdf5-${hdf_version}
 MPI=${MPI} bash ../runconfigure.sh
 make && ${SUDO} make install
-
-cd $ACCESS
 
 # =================== INSTALL PNETCDF (if mpi) ===============
 if [ "$MPI" == "ON" ]
@@ -24,17 +24,17 @@ then
 pnet_version="1.8.1"
 #pnet_version="1.9.0.pre1"
 
+cd $ACCESS
 cd TPL/pnetcdf
 wget http://cucis.ece.northwestern.edu/projects/PnetCDF/Release/parallel-netcdf-${pnet_version}.tar.gz
 tar -xzf parallel-netcdf-${pnet_version}.tar.gz
 cd parallel-netcdf-${pnet_version}
 bash ../runconfigure.sh
 make && ${SUDO} make install
-
-cd $ACCESS
 fi
 
 # =================== INSTALL NETCDF ===============
+cd $ACCESS
 cd TPL/netcdf
 git clone https://github.com/Unidata/netcdf-c netcdf-c
 cd netcdf-c
@@ -43,12 +43,11 @@ cd build
 MPI=${MPI} bash ../../runcmake.sh
 make && ${SUDO} make install
 
-cd $ACCESS
-
 # =================== INSTALL CGNS ===============
 if [ "$CGNS" == "ON" ]
 then
 
+cd $ACCESS
 cd TPL/cgns
 git clone https://github.com/cgns/CGNS
 cd CGNS
@@ -57,20 +56,23 @@ cd build
 MPI=${MPI} bash ../../runconfigure.sh
 make && ${SUDO} make install
 
-cd $ACCESS
-
 fi
 
 # =================== INSTALL MATIO  ===============
 if [ "$MATIO" == "ON" ]
 then
 
+cd $ACCESS
 cd TPL/matio
 git clone https://github.com/tbeu/matio.git
 cd matio
 ./autogen.sh
-sh ../runconfigure.sh
+bash ../runconfigure.sh
 make && ${SUDO} make install
 
-cd $ACCESS
 fi
+
+# ==================================
+cd $ACCESS
+ls -l include
+ls -l lib
