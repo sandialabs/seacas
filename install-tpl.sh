b78615585f104022dadb1df59edcffe1412c188a
Greg Sjaardema <gsjaardema@gmail.com>
2018-01-08 13:06:57 -0700

    Clean up script

--- install-tpl.sh
+++ install-tpl.sh
@@ -3,6 +3,7 @@
 SUDO=${SUDO:-}
 CGNS=${CGNS:-ON}
 MATIO=${MATIO:-ON}
+JOBS=${JOBS:-2}
 ACCESS=`pwd`
 pwd
 
@@ -13,14 +14,16 @@ fi
 
 # =================== INSTALL HDF5 ===============
 hdf_version="1.8.20"
+#hdf_version="1.10.1"
 
 cd $ACCESS
 cd TPL/hdf5
-wget https://support.hdfgroup.org/ftp/HDF5/current18/src/hdf5-${hdf_version}.tar.bz2
+rm -f hdf5-${hdf_version}.tar.bz2
+wget --no-check-certificate https://support.hdfgroup.org/ftp/HDF5/current18/src/hdf5-${hdf_version}.tar.bz2
 tar -jxf hdf5-${hdf_version}.tar.bz2
 cd hdf5-${hdf_version}
 MPI=${MPI} bash ../runconfigure.sh
-make -j2 && ${SUDO} make install
+make -j${JOBS} && ${SUDO} make install
 
 # =================== INSTALL PNETCDF (if mpi) ===============
 if [ "$MPI" == "ON" ]
@@ -31,22 +34,24 @@ pnet_version="1.8.1"
 
 cd $ACCESS
 cd TPL/pnetcdf
+rm -f parallel-netcdf-${pnet_version}.tar.gz
 wget http://cucis.ece.northwestern.edu/projects/PnetCDF/Release/parallel-netcdf-${pnet_version}.tar.gz
 tar -xzf parallel-netcdf-${pnet_version}.tar.gz
 cd parallel-netcdf-${pnet_version}
 bash ../runconfigure.sh
-make -j2 && ${SUDO} make install
+make -j${JOBS} && ${SUDO} make install
 fi
 
 # =================== INSTALL NETCDF ===============
 cd $ACCESS
 cd TPL/netcdf
+rm -rf netcdf-c
 git clone https://github.com/Unidata/netcdf-c netcdf-c
 cd netcdf-c
 mkdir build
 cd build
 MPI=${MPI} bash ../../runcmake.sh
-make -j2 && ${SUDO} make install
+make -j${JOBS} && ${SUDO} make install
 
 # =================== INSTALL CGNS ===============
 if [ "$CGNS" == "ON" ]
@@ -54,12 +59,13 @@ then
 
 cd $ACCESS
 cd TPL/cgns
+rm -rf CGNS
 git clone https://github.com/cgns/CGNS
 cd CGNS
 mkdir build
 cd build
 MPI=${MPI} bash ../../runconfigure.sh
-make -j2 && ${SUDO} make install
+make -j${JOBS} && ${SUDO} make install
 
 fi
 
@@ -69,11 +75,12 @@ then
 
 cd $ACCESS
 cd TPL/matio
+rm -rf matio
 git clone https://github.com/tbeu/matio.git
 cd matio
 ./autogen.sh
 bash ../runconfigure.sh
-make -j2 && ${SUDO} make install
+make -j${JOBS} && ${SUDO} make install
 
 fi
 
