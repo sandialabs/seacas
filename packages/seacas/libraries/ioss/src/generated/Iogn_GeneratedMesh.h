e65220decc9f38ab1aaf4bbcd77a20457ae22930
Greg Sjaardema <gdsjaar@sandia.gov>
2018-01-30 21:42:28 -0700

    IOSS: generated - fix owningprocessor map building

--- packages/seacas/libraries/ioss/src/generated/Iogn_GeneratedMesh.h
+++ packages/seacas/libraries/ioss/src/generated/Iogn_GeneratedMesh.h
@@ -362,7 +362,7 @@ namespace Iogn {
     virtual std::pair<std::string, int> topology_type(int64_t block_number) const;
 
     void            build_node_map(Ioss::Int64Vector &map, std::vector<int> &proc, int64_t slab,
-                                   size_t slabOffset, size_t adjacentProc);
+                                   size_t slabOffset, size_t adjacentProc, size_t index);
     virtual int64_t communication_node_count_proc() const;
     virtual void    node_communication_map(MapVector &map, std::vector<int> &proc);
     virtual void    owning_processor(int *owner, int64_t num_node);
