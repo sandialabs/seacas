f3625f6c1e2d27d21e56b5443860cec779ad0371
Greg Sjaardema <gsjaardema@gmail.com>
2018-02-14 15:42:21 -0700

    CONJOIN: Add option to ignore coordinates when uniquifying part nodes

--- packages/seacas/applications/conjoin/CJ_SystemInterface.C
+++ packages/seacas/applications/conjoin/CJ_SystemInterface.C
@@ -15,8 +15,6 @@ namespace {
 } // namespace
 
 Excn::SystemInterface::SystemInterface()
-    : outputName_(), debugLevel_(0), screenWidth_(0), omitNodesets_(false), omitSidesets_(false),
-      ints64Bit_(false), aliveValue_(-1.0), interpartMinimumTimeDelta_(0.0)
 {
   enroll_options();
 }
@@ -58,6 +56,10 @@ void Excn::SystemInterface::enroll_options()
 
   options_.enroll("64-bit", GetLongOption::NoValue,
                   "True if forcing the use of 64-bit integers for the output file", nullptr);
+
+  options_.enroll("ignore_coordinate_check", GetLongOption::NoValue,
+                  "Do not use nodal coordiantes to determine if node in part 1 same as node in other parts; use ids only.\n"
+		  "\t\tUse only if you know that the meshes are the same for all parts", nullptr);
 
   options_.enroll("omit_nodesets", GetLongOption::NoValue,
                   "Don't transfer nodesets to output file.", nullptr);
@@ -238,6 +240,10 @@ bool Excn::SystemInterface::parse_options(int argc, char **argv)
 
   if (options_.retrieve("64-bit") != nullptr) {
     ints64Bit_ = true;
+  }
+
+  if (options_.retrieve("ignore_coordinate_check") != nullptr) {
+    ignoreCoordinates_ = true;
   }
 
   if (options_.retrieve("omit_nodesets") != nullptr) {
