4f77d2316cab667da86ea6da2a84bac9f4e075fc
Greg Sjaardema <gsjaardema@gmail.com>
2018-04-11 15:43:56 -0600

    IOSS: cgns - add testing cases

--- packages/seacas/libraries/ioss/src/utest/Utst_structured_decomp.C
+++ packages/seacas/libraries/ioss/src/utest/Utst_structured_decomp.C
@@ -62,7 +62,7 @@ TEST_CASE("test prime sides", "[prime_sides]")
       std::accumulate(zones.begin(), zones.end(), 0.0,
                       [](double a, Iocgns::StructuredZoneData *b) { return a + b->work(); });
 
-  double load_balance_tolerance = 1.1;
+  double load_balance_tolerance = 1.25;
   for (size_t proc_count = 2; proc_count < 8; proc_count++) {
     std::string name = "Prime_ProcCount_" + std::to_string(proc_count);
     SECTION(name)
@@ -75,6 +75,34 @@ TEST_CASE("test prime sides", "[prime_sides]")
       for (const auto zone : zones) {
         if (zone->is_active()) {
           CHECK(zone->work() <= max_work);
+        }
+      }
+      SECTION("assign_to_procs")
+      {
+        std::vector<size_t> work_vector(proc_count);
+        Iocgns::Utils::assign_zones_to_procs(zones, work_vector);
+
+        // Each active zone must be on a processor
+        for (const auto zone : zones) {
+          if (zone->is_active()) {
+            CHECK(zone->m_proc >= 0);
+          }
+        }
+
+        // Work must be min_work <= work <= max_work
+        for (auto work : work_vector) {
+          // CHECK(work >= min_work);
+          CHECK(work <= max_work);
+        }
+
+        // A processor cannot have more than one zone with the same adam zone
+        std::set<std::pair<int, int>> proc_adam_map;
+        for (size_t i = 0; i < zones.size(); i++) {
+          if (zones[i]->is_active()) {
+            auto success =
+                proc_adam_map.insert(std::make_pair(zones[i]->m_adam->m_zone, zones[i]->m_proc));
+            CHECK(success.second);
+          }
         }
       }
     }
