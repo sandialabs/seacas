8e82f9d0330dd4123e5237663598be1e3435b6a6
gsjaardema <gsjaardema@gmail.com>
2018-06-27 08:57:05 -0400

    Add test of exodus.py copy function

--- SEACAS-Test/testExo.py
+++ SEACAS-Test/testExo.py
@@ -60,3 +60,51 @@ for array_type in array_types:
     new_database_path = database_path[:-2] + '_' + array_type + '_copy.e'
     exodus.copyTransfer(database_path, new_database_path, array_type = array_type)
     print "Database copied using " + array_type + " arrays."
+
+#Test the exodus.py `copy` function which calls the C API `ex_copy`
+e = exodus.exodus(database_path)
+new_database_path = database_path[:-2] + '_copy.e'
+exo_copy = e.copy(new_database_path)
+
+print "Exodus file has title:", exo_copy.title()
+print "Exodus file has", exo_copy.num_dimensions(), "dimensions"
+print "Exodus file has", exo_copy.num_nodes(), "nodes"
+print "Exodus file has", exo_copy.num_elems(), "elements"
+print "Exodus file has", exo_copy.num_blks(), "blocks"
+print "Exodus file has", exo_copy.num_node_sets(), "node sets"
+print "Exodus file has", exo_copy.num_side_sets(), "side sets"
+print "Exodus file has", exo_copy.num_times(), "time steps"
+if exo_copy.num_times() > 0:
+    times = exo_copy.get_times()
+    for time in times:
+        print "time = ", time
+        
+    blocks = exo_copy.get_elem_blk_ids()
+    for block in blocks:
+        print "block id = ", block
+        sidesets = exo_copy.get_side_set_ids()
+    for sideset in sidesets:
+        print "side set id = ", sideset
+        nodesets = exo_copy.get_node_set_ids()
+    for nodeset in nodesets:
+        print "node set id = ", nodeset
+        coordinates = exo_copy.get_coords()
+        print "Local Node Id 1 has coordinates:", coordinates[0][0], coordinates[1][0], coordinates[2][0]
+        nn = (exo_copy.num_nodes() - 1)
+        print "Local Node Id", exo_copy.num_nodes(), "has coordinates:", coordinates[0][nn], coordinates[1][nn], coordinates[2][nn] 
+        print "Side Set Variable Names"
+        ssVarNames = exo_copy.get_side_set_variable_names()
+    for name in ssVarNames:
+        print "ssvar = " + name
+        print "Side Set Cosa Variable Values"
+        step = 1
+                        
+        if exo_copy.num_times() > 0:
+            for time in times:
+                print "time = ", time
+            ssvals = exo_copy.get_side_set_variable_values(1,"cosa",step)
+            for ssval in ssvals:
+                print "value =", ssval
+                step += 1
+                                    
+exo_copy.close()
