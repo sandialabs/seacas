b23b7c9414e285ef261232b225d215254bebea5b
Greg Sjaardema <gsjaardema@gmail.com>
2018-09-27 10:33:03 -0600

    EXODUS: Changes needed for NetCDF in-memory changes

--- packages/seacas/libraries/exodus/src/ex_create_par.c
+++ packages/seacas/libraries/exodus/src/ex_create_par.c
@@ -451,6 +451,8 @@ int ex_create_par_int(const char *path, int cmode, int *comp_ws, int *io_ws, MPI
     mode_name = "NOCLOBBER";
   }
 
+  nc_mode |= NC_WRITE;
+
   if ((status = nc_create_par(path, nc_mode | pariomode, comm, info, &exoid)) != NC_NOERR) {
 #if NC_HAS_HDF5
     snprintf(errmsg, MAX_ERR_LENGTH, "ERROR: file create failed for %s, mode: %s", path, mode_name);
