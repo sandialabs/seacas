5b544734eb9c29a5b64da27bb998a2e3ddf6afa8
Greg Sjaardema <gsjaardema@gmail.com>
2018-10-25 18:57:15 -0600

    Doxygen update

--- docs/html/search/variables_12.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['valid_5fids',['valid_ids',['../structobj__stats.html#abe12ee8b8ad80d32bc9c2044e2cd2386',1,'obj_stats']]],
-  ['valid_5fstat',['valid_stat',['../structobj__stats.html#a20d1d15cd2518ae027ac6daf43370c33',1,'obj_stats']]],
-  ['value',['value',['../structlist__item.html#a942b621521bcef52f811123db796dafc',1,'list_item::value()'],['../exo__jack_8c.html#a73e4c1f8a3b3cc1b7289e19f419dda13',1,'value():&#160;exo_jack.c']]],
-  ['values',['values',['../exo__jack_8c.html#ab629d36b51dffd5414610d9ada2b2e81',1,'exo_jack.c']]],
-  ['version',['version',['../exo__jack_8c.html#a6d293416d66533364bc7f94eac21b232',1,'exo_jack.c']]]
-];
--- docs/html/search/variables_13.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['warning_5foutput',['warning_output',['../ex__open_8c.html#ab71aa5cfa846f02a7fb1924bcfeb77db',1,'warning_output():&#160;ex_open.c'],['../ex__utils_8c.html#ab71aa5cfa846f02a7fb1924bcfeb77db',1,'warning_output():&#160;ex_utils.c']]]
-];
