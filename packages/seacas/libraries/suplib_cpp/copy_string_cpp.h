8b856192c0fe592fd2ad766b266f8429a0d34811
Greg Sjaardema <gsjaardema@gmail.com>
2019-04-16 15:48:04 -0600

    Add missing copy_string C++ include

---
+++ packages/seacas/libraries/suplib_cpp/copy_string_cpp.h
@@ -0,0 +1,19 @@
+#ifndef copy_string_cpp_h
+#define copy_string_cpp_h
+#include <copy_string.h>
+#include <string>
+
+template <size_t size>
+void copy_string(char (&output)[size], const char* source)
+{
+  // Copy the string — don’t copy too many bytes.
+  copy_string(output, source, size);
+}
+
+template <size_t size>
+void copy_string(char (&output)[size], const std::string &source)
+{
+  // Copy the string — don’t copy too many bytes.
+  copy_string(output, source.c_str(), size);
+}
+#endif
