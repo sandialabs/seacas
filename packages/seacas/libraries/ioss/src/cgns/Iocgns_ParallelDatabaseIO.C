af0fd6074e79f92418c3abb8c666626a52dc63d2
Greg Sjaardema <gdsjaar@sandia.gov>
2019-05-16 12:32:47 -0600

    IOSS: Fix appending to parallel cgns file

--- packages/seacas/libraries/ioss/src/cgns/Iocgns_ParallelDatabaseIO.C
+++ packages/seacas/libraries/ioss/src/cgns/Iocgns_ParallelDatabaseIO.C
@@ -304,9 +304,10 @@ namespace Iocgns {
   void ParallelDatabaseIO::openSerialDatabase__() const
   {
     if (m_cgnsSerFilePtr < 0) {
-      if (myProcessor == 0 && is_input()) {
+      if (myProcessor == 0 && (is_input() || open_create_behavior() == Ioss::DB_APPEND)) {
         auto init           = pcg_mpi_initialized;
         pcg_mpi_initialized = 0;
+	// Even if appending, the serial file is only read, not written, so CG_MODE_READ is ok.
         cg_open(get_filename().c_str(), CG_MODE_READ, &m_cgnsSerFilePtr);
         pcg_mpi_initialized = init;
         assert(m_cgnsSerFilePtr >= 0);
