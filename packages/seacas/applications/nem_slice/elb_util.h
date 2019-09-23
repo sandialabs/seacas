77af9fbedef30dd9763dd13445c5967f192dd9b7
Greg Sjaardema <gsjaardema@gmail.com>
2019-09-23 13:37:47 -0600

    Fixes for ssize_t

--- packages/seacas/applications/nem_slice/elb_util.h
+++ packages/seacas/applications/nem_slice/elb_util.h
@@ -41,8 +41,11 @@
 #include <vector>
 
 #if defined(_MSC_VER)
-#include <BaseTsd.h>
-typedef SSIZE_T ssize_t;
+#ifdef _WIN64
+#define ssize_t __int64
+#else
+#define ssize_t long
+#endif
 #endif
 
 /* Function prototypes */
