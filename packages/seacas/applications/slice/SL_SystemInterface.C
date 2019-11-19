de13f0b8cd7445c0c6979cbc37c43bf13cf9111e
Greg Sjaardema <gdsjaar@sandia.gov>
2019-11-19 08:08:36 -0700

    SLICE: Fix incorrect equal cal

--- packages/seacas/applications/slice/SL_SystemInterface.C
+++ packages/seacas/applications/slice/SL_SystemInterface.C
@@ -56,7 +56,7 @@ namespace {
   bool str_equal(const std::string &s1, const std::string &s2)
   {
     return (s1.size() == s2.size()) &&
-           std::equal(s1.begin(), s1.end(), s2.begin(), s2.end(),
+           std::equal(s1.begin(), s1.end(), s2.begin(),
                       [](char a, char b) { return std::tolower(a) == std::tolower(b); });
   }
 
