557eabe4258846a5abbc0346ea950909fdc6ce76
Greg Sjaardema <gdsjaar@sandia.gov>
2019-12-02 10:06:22 -0700

    Sync from Trilinos

--- packages/seacas/libraries/ioss/src/fmt/core.h
+++ packages/seacas/libraries/ioss/src/fmt/core.h
@@ -69,8 +69,7 @@
 #ifndef FMT_USE_CONSTEXPR
 #define FMT_USE_CONSTEXPR                                                                          \
   (FMT_HAS_FEATURE(cxx_relaxed_constexpr) || FMT_MSC_VER >= 1910 ||                                \
-   (FMT_GCC_VERSION >= 600 && __cplusplus >= 201402L)) &&                                          \
-      !FMT_NVCC
+   (FMT_GCC_VERSION >= 600 && __cplusplus >= 201402L))
 #endif
 #if FMT_USE_CONSTEXPR
 #define FMT_CONSTEXPR constexpr
@@ -241,7 +240,7 @@ namespace internal {
 
 #ifdef FMT_USE_INT128
 // Do nothing.
-#elif defined(__SIZEOF_INT128__)
+#elif defined(__SIZEOF_INT128__) && !FMT_NVCC
 #define FMT_USE_INT128 1
   using int128_t                                 = __int128_t;
   using uint128_t                                = __uint128_t;
@@ -1426,9 +1425,11 @@ namespace internal {
   inline format_arg_store<buffer_context<Char>, remove_reference_t<Args>...>
   make_args_checked(const S &format_str, const remove_reference_t<Args> &... args)
   {
+#ifndef __INTEL_COMPILER
     static_assert(all_true<(!std::is_base_of<view, remove_reference_t<Args>>() ||
                             !std::is_reference<Args>())...>::value,
                   "passing views as lvalues is disallowed");
+#endif
     check_format_string<remove_const_t<remove_reference_t<Args>>...>(format_str);
     return {args...};
   }
