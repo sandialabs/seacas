d6636180f8847e8be87ed86826ff4a77bccbef85
Greg Sjaardema <gsjaardema@gmail.com>
2020-01-06 10:57:45 -0700

    EXODUS.PY: Fix is vs ==

--- packages/seacas/scripts/exodus3.in.py
+++ packages/seacas/scripts/exodus3.in.py
@@ -1,5 +1,5 @@
 """
-exodus.py v 1.18.1 (seacas-beta) is a python wrapper of some of the exodus library
+exodus.py v 1.18.2 (seacas-beta) is a python wrapper of some of the exodus library
 (Python 3 Version)
 
 Exodus is a common database for multiple application codes (mesh
@@ -95,10 +95,10 @@ from enum import Enum
 
 EXODUS_PY_COPYRIGHT_AND_LICENSE = __doc__
 
-EXODUS_PY_VERSION = "1.18.1 (seacas-py3)"
+EXODUS_PY_VERSION = "1.18.2 (seacas-py3)"
 
 EXODUS_PY_COPYRIGHT = """
-You are using exodus.py v 1.18.1 (seacas-py3), a python wrapper of some of the exodus library.
+You are using exodus.py v 1.18.2 (seacas-py3), a python wrapper of some of the exodus library.
 
 Copyright (c) 2013, 2014, 2015, 2016, 2017, 2018, 2019 National Technology &
 Engineering Solutions of Sandia, LLC (NTESS).  Under the terms of
@@ -5776,7 +5776,7 @@ def add_variables(exo, global_vars=[], nodal_vars=[], element_vars=[], node_set_
     if not isinstance(side_set_vars, list):
         raise Exception("ERROR: side_set_vars is not a list.")
 
-    if exo.modeChar is 'r':
+    if exo.modeChar == 'r':
         raise Exception(
             "ERROR: variables cannot be added to an exodus object in read only mode")
 
