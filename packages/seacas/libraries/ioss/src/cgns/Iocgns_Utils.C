98056a876d6ca0e032c741b1073b8f1eb312bcd5
Greg Sjaardema <gdsjaar@sandia.gov>
2020-01-28 12:41:46 -0700

    IOSS: Fix fpp read to handle periodic blocks

--- packages/seacas/libraries/ioss/src/cgns/Iocgns_Utils.C
+++ packages/seacas/libraries/ioss/src/cgns/Iocgns_Utils.C
@@ -80,29 +80,6 @@
   }
 
 namespace {
-  std::pair<std::string, int> decompose_name(const std::string &name, bool is_parallel)
-  {
-    int         proc = 0;
-    std::string zname{name};
-
-    if (is_parallel) {
-      // Name should/might be of the form `basename_proc-#`.  Strip
-      // off the `_proc-#` portion and return just the basename.
-      auto tokens = Ioss::tokenize(zname, "_");
-      zname       = tokens[0];
-      if (tokens.size() >= 2) {
-        size_t idx = tokens.size() - 1;
-        if (tokens[idx].substr(0, 5) == "proc-") {
-          auto ptoken = Ioss::tokenize(tokens[idx], "-");
-          proc        = std::stoi(ptoken[1]);
-          idx--;
-          zname = tokens[idx];
-        }
-      }
-    }
-    return std::make_pair(zname, proc);
-  }
-
   int power_2(int count)
   {
     // Return the maximum power of two which is less than or equal to 'count'
@@ -383,6 +360,29 @@ namespace {
     }
   }
 } // namespace
+
+std::pair<std::string, int> Iocgns::Utils::decompose_name(const std::string &name, bool is_parallel)
+  {
+    int         proc = is_parallel ? -1 : 0;
+    std::string zname{name};
+
+    if (is_parallel) {
+      // Name should/might be of the form `basename_proc-#`.  Strip
+      // off the `_proc-#` portion and return just the basename.
+      auto tokens = Ioss::tokenize(zname, "_");
+      zname       = tokens[0];
+      if (tokens.size() >= 2) {
+        size_t idx = tokens.size() - 1;
+        if (tokens[idx].substr(0, 5) == "proc-") {
+          auto ptoken = Ioss::tokenize(tokens[idx], "-");
+          proc        = std::stoi(ptoken[1]);
+          idx--;
+          zname = tokens[idx];
+        }
+      }
+    }
+    return std::make_pair(zname, proc);
+  }
 
 template <typename INT>
 void Iocgns::Utils::map_cgns_connectivity(const Ioss::ElementTopology *topo, size_t element_count,
