15f5877faa513a90306b08abaf848061aff6c30c
Greg Sjaardema <gsjaardema@gmail.com>
2020-03-16 08:22:26 -0600

    clang-tidy fixes

--- packages/seacas/libraries/ioss/src/main/io_info.C
+++ packages/seacas/libraries/ioss/src/main/io_info.C
@@ -1,4 +1,4 @@
-// Copyright(C) 1999-2017 National Technology & Engineering Solutions
+// Copyright(C) 1999-2017, 2020 National Technology & Engineering Solutions
 // of Sandia, LLC (NTESS).  Under the terms of Contract DE-NA0003525 with
 // NTESS, the U.S. Government retains certain rights in this software.
 //
@@ -597,7 +597,7 @@ namespace Ioss {
         }
         region.output_summary(std::cout, true);
 
-        if (interFace.summary() == 0) {
+        if (!interFace.summary()) {
 
           info_nodeblock(region, interFace);
           info_edgeblock(region);
