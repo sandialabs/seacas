acc2b633dc84d54dce37d61aec6ca289945456c9
Greg Sjaardema <gsjaardema@gmail.com>
2020-03-25 10:22:35 -0600

    IOSS: Clean up error message

--- packages/seacas/libraries/ioss/src/Ioss_FaceGenerator.C
+++ packages/seacas/libraries/ioss/src/Ioss_FaceGenerator.C
@@ -331,8 +331,9 @@ namespace Ioss {
   {
     std::ostringstream errmsg;
     fmt::print(errmsg,
-               "ERROR: Face {} has more than two elements using it. The element/local_face are: "
-               "{}:{}, {}:{}, and {}:{}.  The face connectivity is {} {} {} {}.\n",
+               "ERROR: Face {} has more than two elements using it.\n"
+               "       The element/local_face are: {}:{}, {}:{}, and {}:{}.\n"
+               "       The face connectivity is {} {} {} {}.\n",
                hashId_, element[0] / 10, element[0] % 10, element[1] / 10, element[1] % 10,
                element_id / 10, element_id % 10, connectivity_[0], connectivity_[1],
                connectivity_[2], connectivity_[3]);
