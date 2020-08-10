d0f05d3dcf5b07b39b8507e5d41a8bc1fd20c4df
Greg Sjaardema <gsjaardema@gmail.com>
2020-08-10 11:40:36 -0600

    EXODIFF: Add coordinate_tolerance option to set tolerance for coordinate comparisons

--- packages/seacas/applications/exodiff/ED_Version.h
+++ packages/seacas/applications/exodiff/ED_Version.h
@@ -6,7 +6,7 @@
 #ifndef ED_Version_h
 #define ED_Version_h
 
-static std::string version("3.04");
-static std::string verdate("2020-08-07");
+static std::string version("3.05");
+static std::string verdate("2020-08-10");
 
 #endif // ED_Version_h
