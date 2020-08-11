1a2c3825af067794a46370fe6e4648638373f0ed
Greg Sjaardema <gsjaardema@gmail.com>
2020-08-11 11:19:37 -0600

    TRIBITS: Snapshot current TriBITs version

--- cmake/tribits/core/package_arch/TribitsGenerateResourceSpecFile.cmake
+++ cmake/tribits/core/package_arch/TribitsGenerateResourceSpecFile.cmake
@@ -2,7 +2,7 @@
 # ************************************************************************
 #
 #            TriBITS: Tribal Build, Integrate, and Test System
-#                    Copyright 2016 Sandia Corporation
+#                    Copyright 2013 Sandia Corporation
 #
 # Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
 # the U.S. Government retains certain rights in this software.
@@ -37,49 +37,58 @@
 # ************************************************************************
 # @HEADER
 
-#
-# First, set up the variables for the (backward-compatible) TriBITS way of
-# finding Netcdf.  These are used in case FIND_PACKAGE(NetCDF ...) is not
-# called or does not find NetCDF.  Also, these variables need to be non-null
-# in order to trigger the right behavior in the function
-# TRIBITS_TPL_FIND_INCLUDE_DIRS_AND_LIBRARIES().
-#
-if (${CMAKE_VERSION} GREATER "3.13")
-     cmake_policy(SET CMP0074 NEW)
-endif()
-
-SET(REQUIRED_HEADERS cgnslib.h)
-SET(REQUIRED_LIBS_NAMES cgns)
 
 #
-# Second, search for Netcdf components (if allowed) using the standard
-# FIND_PACKAGE(CGNS ...).
+# Top-level project logic to generate resources spec file
 #
-TRIBITS_TPL_ALLOW_PRE_FIND_PACKAGE(CGNS  CGNS_ALLOW_PREFIND)
-IF (CGNS_ALLOW_PREFIND)
-
-  MESSAGE("-- Using FIND_PACKAGE(CGNS ...) ...")
-
-  SET(CMAKE_MODULE_PATH
-    "${CMAKE_MODULE_PATH}"
-    "${CMAKE_CURRENT_LIST_DIR}/find_modules"
-    "${CMAKE_CURRENT_LIST_DIR}/utils"
-     )
-  
-  find_package(CGNS)
-
-  IF (CGNS_FOUND)
-    set(TPL_CGNS_LIBRARIES ${CGNS_LIBRARIES} CACHE PATH
-      "List of semi-colon seprated (full) paths to the CGNS libraries")
-    set(TPL_CGNS_INCLUDE_DIRS ${CGNS_INCLUDE_DIRS} CACHE PATH
-      "List of semi-colon seprated list of directories containing CGNS header files")
+FUNCTION(TRIBITS_GENERATE_CTEST_RESOURCE_SPEC_FILE_PROJECT_LOGIC)
+  IF (${PROJECT_NAME}_AUTOGENERATE_TEST_RESOURCE_FILE)
+    IF (CTEST_RESOURCE_SPEC_FILE STREQUAL CTEST_RESOURCE_SPEC_FILE_DEFAULT)
+      TRIBITS_GENERATE_CTEST_RESOURCE_SPEC_FILE()
+    ELSE()
+      MESSAGE("NOTE: The test resource file CTEST_RESOURCE_SPEC_FILE='${CTEST_RESOURCE_SPEC_FILE}'"
+        " will not be auto-generated even through"
+        " ${PROJECT_NAME}_AUTOGENERATE_TEST_RESOURCE_FILE=${${PROJECT_NAME}_AUTOGENERATE_TEST_RESOURCE_FILE}"
+        " because its location does not match the default"
+        " location '${CTEST_RESOURCE_SPEC_FILE_DEFAULT}'."
+        "  If you want to auto-generate this file, please clear CTEST_RESOURCE_SPEC_FILE and"
+        " reconfigure or create that file on your own and clear"
+        " ${PROJECT_NAME}_AUTOGENERATE_TEST_RESOURCE_FILE."
+        )
+    ENDIF()
   ENDIF()
+ENDFUNCTION()
 
-ENDIF()
 
 #
-# Third, call TRIBITS_TPL_FIND_INCLUDE_DIRS_AND_LIBRARIES()
+# Generate resource spec file
 #
-TRIBITS_TPL_FIND_INCLUDE_DIRS_AND_LIBRARIES( CGNS
-  REQUIRED_HEADERS ${REQUIRED_HEADERS}
-  REQUIRED_LIBS_NAMES ${REQUIRED_LIBS_NAMES})
+FUNCTION(TRIBITS_GENERATE_CTEST_RESOURCE_SPEC_FILE)
+  SET(GPUS_JSON)
+  MATH(EXPR LAST_GPU "${${PROJECT_NAME}_CUDA_NUM_GPUS} - 1")
+  SET(FIRST 1)
+  FOREACH(GPU RANGE 0 ${LAST_GPU})
+    IF(NOT FIRST)
+      STRING(APPEND GPUS_JSON ",\n")
+    ENDIF()
+    SET(FIRST 0)
+    STRING(APPEND GPUS_JSON "        {
+          \"id\": \"${GPU}\",
+          \"slots\": ${${PROJECT_NAME}_CUDA_SLOTS_PER_GPU}
+        }")
+  ENDFOREACH()
+  FILE(WRITE "${CMAKE_BINARY_DIR}/ctest_resources.json" "{
+  \"version\": {
+    \"major\": 1,
+    \"minor\": 0
+  },
+  \"local\": [
+    {
+      \"gpus\": [
+${GPUS_JSON}
+      ]
+    }
+  ]
+}
+")
+ENDFUNCTION()
