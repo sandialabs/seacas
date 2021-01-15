74e16bc3596f835dd93ec149384dee289b4db455
Greg Sjaardema <gsjaardema@gmail.com>
2021-01-15 16:56:41 -0700

    IOSS: See if fixes windows build

--- packages/seacas/libraries/ioss/src/Ioss_Utils.C
+++ packages/seacas/libraries/ioss/src/Ioss_Utils.C
@@ -1,4 +1,4 @@
-// Copyright(C) 1999-2020 National Technology & Engineering Solutions
+// Copyright(C) 1999-2021 National Technology & Engineering Solutions
 // of Sandia, LLC (NTESS).  Under the terms of Contract DE-NA0003525 with
 // NTESS, the U.S. Government retains certain rights in this software.
 //
@@ -46,7 +46,6 @@
 
 // For memory utilities...
 #if defined(_WIN32)
-#include <psapi.h>
 #include <windows.h>
 #undef max
 
