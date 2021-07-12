d07544adf994df4a525774d01febb4fb59c7bd63
Greg Sjaardema <gdsjaar@sandia.gov>
2021-07-12 11:20:20 -0600

    IOSS: check for chdir failure in parallel file open

--- packages/seacas/libraries/ioss/src/exodus/Ioex_ParallelDatabaseIO.C
+++ packages/seacas/libraries/ioss/src/exodus/Ioex_ParallelDatabaseIO.C
@@ -454,7 +454,28 @@ namespace Ioex {
     std::string    path = file.pathname();
     filename            = file.tailname();
     char *current_cwd   = getcwd(nullptr, 0);
-    chdir(path.c_str());
+    auto success = chdir(path.c_str());
+    if (success == -1) {
+      if (write_message || error_msg != nullptr) {
+	std::ostringstream errmsg;
+	fmt::print(errmsg, "ERROR: Directory '{}' does not exist.  Error in filename specification.",
+		   path);
+	fmt::print(errmsg, "\n");
+	if (error_msg != nullptr) {
+	  *error_msg = errmsg.str();
+	}
+	if (write_message && myProcessor == 0) {
+	  fmt::print(Ioss::OUTPUT(), "{}", errmsg.str());
+	}
+        if (bad_count != nullptr) {
+          *bad_count = 1;
+        }
+        if (abort_if_error) {
+          IOSS_ERROR(errmsg);
+        }
+      }
+      return false;
+    }
 #endif
 
     bool do_timer = false;
