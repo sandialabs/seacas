37ee0748d1efd0c29adadbd33b2c3e0245183104
Greg Sjaardema <gsjaardema@gmail.com>
2021-08-03 14:40:51 -0600

    IOSS: Fix missing functions on windows for Iovs

--- packages/seacas/libraries/ioss/src/visualization/utils/Iovs_Utils.C
+++ packages/seacas/libraries/ioss/src/visualization/utils/Iovs_Utils.C
@@ -15,13 +15,15 @@
 #include <dlfcn.h>
 #endif
 
+#ifndef _WIN32
 #include <libgen.h>
+#endif
+
 #include <sys/stat.h>
 
 #ifdef _WIN32
 #include <windows.h>
 #endif
-
 
 namespace Iovs {
   std::string persistentLdLibraryPathEnvForCatalyst = "";
@@ -385,7 +387,7 @@ namespace Iovs {
 #ifdef _WIN32
     char *cbuf = _fullpath(nullptr, sierraInsDir.c_str(), _MAX_PATH);
 #else
-    char *cbuf = realpath(sierraInsDir.c_str(), nullptr);
+    char *cbuf  = realpath(sierraInsDir.c_str(), nullptr);
 #endif
     std::string sierraInsPath = cbuf;
     free(cbuf);
@@ -398,6 +400,13 @@ namespace Iovs {
       IOSS_ERROR(errmsg);
     }
 
+#ifdef _WIN32
+    {
+      std::ostringstream errmsg;
+      errmsg << "This code is not yet supported on windows...\n";
+      IOSS_ERROR(errmsg);
+    }
+#else
     char *cbase = strdup(sierraInsPath.c_str());
     char *cdir  = strdup(sierraInsPath.c_str());
     char *bname = basename(cbase);
@@ -407,6 +416,7 @@ namespace Iovs {
       bname = basename(dname);
       dname = dirname(dname);
     }
+#endif
 
     if (strcmp(bname, "sierra") == 0) {
       sierraInsPath = dname;
