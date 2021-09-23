5f813f71868e1784ce041a0573a8a0dfa5f7bdf7
Greg Sjaardema <gsjaardema@gmail.com>
2021-09-23 12:29:40 -0600

    APREPRO: Another potential mingw windows build fix

--- packages/seacas/libraries/aprepro_lib/apr_util.cc
+++ packages/seacas/libraries/aprepro_lib/apr_util.cc
@@ -1,4 +1,4 @@
-// Copyright(C) 1999-2020 National Technology & Engineering Solutions
+// Copyright(C) 1999-2021 National Technology & Engineering Solutions
 // of Sandia, LLC (NTESS).  Under the terms of Contract DE-NA0003525 with
 // NTESS, the U.S. Government retains certain rights in this software.
 //
@@ -22,7 +22,9 @@
 #ifdef _WIN32
 #include <fcntl.h>
 #include <io.h>
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
 #include <windows.h>
 #else
 #include <unistd.h> // for close
