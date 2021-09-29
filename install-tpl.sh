94724a535a2c52ce96df6fe7dd1be98125f3d738
Greg Sjaardema <gsjaardema@gmail.com>
2021-09-29 10:17:25 -0600

    Use pnetcdf-1.12.2

--- install-tpl.sh
+++ install-tpl.sh
@@ -459,7 +459,7 @@ then
     if [ "$FORCE" == "YES" ] || ! [ -e $INSTALL_PATH/lib/libpnetcdf.a ]
     then
         echo "${txtgrn}+++ PnetCDF${txtrst}"
-        pnet_version="1.12.1"
+        pnet_version="1.12.2"
 	pnet_base="pnetcdf"
         cd $ACCESS
         cd TPL/pnetcdf
