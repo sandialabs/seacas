ed44c17193007e1c83dd924b035f29c56fa107a8
Greg Sjaardema <gsjaardema@gmail.com>
2021-11-11 10:56:56 -0700

    CI: Remove some debug output

--- ci-msys2-build.sh
+++ ci-msys2-build.sh
@@ -7,5 +7,5 @@ mkdir build
 cd build
 
 HAVE_X11=NO NETCDF_PATH=/mingw64 HDF5_PATH=/mingw64 CGNS_PATH=/mingw64 MPI=NO bash ../cmake-config
-make VERBOSE=1 -j4
+make -j4
 #ctest -j 4 --output-on-failure
