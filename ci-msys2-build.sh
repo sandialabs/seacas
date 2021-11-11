8a87e8dcc00325304740d56c46a0e65c6f100ec5
Greg Sjaardema <gsjaardema@gmail.com>
2021-11-11 09:12:31 -0700

    CI: Try no fortran build first...

--- ci-msys2-build.sh
+++ ci-msys2-build.sh
@@ -6,6 +6,6 @@ echo $HOME
 mkdir build
 cd build
 
-HAVE_X11=NO NETCDF_PATH=/mingw64 HDF5_PATH=/mingw64 CGNS_PATH=/mingw64 MPI=NO bash ../cmake-config
+FORTRAN=NO HAVE_X11=NO NETCDF_PATH=/mingw64 HDF5_PATH=/mingw64 CGNS_PATH=/mingw64 MPI=NO bash ../cmake-config
 make -j4
 ctest -j 4 --output-on-failure
