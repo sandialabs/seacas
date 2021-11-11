39422cb7f73419992af2ddbc70dcd28e52a774fe
Greg Sjaardema <gsjaardema@gmail.com>
2021-11-11 14:56:12 -0700

    CI: See if any tests run on msys2

--- ci-msys2-build.sh
+++ ci-msys2-build.sh
@@ -8,4 +8,4 @@ cd build
 
 HAVE_X11=NO NETCDF_PATH=/mingw64 HDF5_PATH=/mingw64 CGNS_PATH=/mingw64 MPI=NO bash ../cmake-config
 make -k -j4
-#ctest -j 4 --output-on-failure
+ctest -j 4 --output-on-failure
