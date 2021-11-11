f7faae059fb3dddff9359520570e315cd8649b0d
Greg Sjaardema <gsjaardema@gmail.com>
2021-11-11 09:43:08 -0700

    CI: Add fortran, no tests, verbose build

--- ci-msys2-build.sh
+++ ci-msys2-build.sh
@@ -6,6 +6,6 @@ echo $HOME
 mkdir build
 cd build
 
-FORTRAN=NO HAVE_X11=NO NETCDF_PATH=/mingw64 HDF5_PATH=/mingw64 CGNS_PATH=/mingw64 MPI=NO bash ../cmake-config
-make -j4
-ctest -j 4 --output-on-failure
+HAVE_X11=NO NETCDF_PATH=/mingw64 HDF5_PATH=/mingw64 CGNS_PATH=/mingw64 MPI=NO bash ../cmake-config
+make VERBOSE=1 -j4
+#ctest -j 4 --output-on-failure
