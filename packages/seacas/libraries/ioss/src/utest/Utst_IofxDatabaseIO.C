6a444a7023d3ba4639255fbe4d5ab1d38eb57401
Greg Sjaardema <gsjaardema@gmail.com>
2021-12-17 10:01:51 -0700

    IOSS: New version of get_block_adjacencies function

--- packages/seacas/libraries/ioss/src/utest/Utst_IofxDatabaseIO.C
+++ packages/seacas/libraries/ioss/src/utest/Utst_IofxDatabaseIO.C
@@ -98,8 +98,7 @@ namespace {
     // element block testing
 
     for (size_t i = 0; i < element_blocks.size(); ++i) {
-      std::vector<std::string> empty;
-      db_io->get_block_adjacencies(element_blocks[i], empty);
+      std::vector<std::string> empty = element_blocks[i]->get_block_adjacencies();
       CHECK(!empty.empty());
       CHECK(gold_strings[i] == empty[0]);
 
@@ -140,7 +139,7 @@ namespace {
                                          0.5,  0.5,  -0.5, -0.5, 0.5,  0.5, 0.5,  -0.5, -0.5,
                                          0.5,  0.5,  -0.5, -0.5, 0.5,  0.5, -0.5, -0.5, 0.5};
 
-    Ioss::NodeBlock *   nb = region.get_node_blocks()[0];
+    Ioss::NodeBlock    *nb = region.get_node_blocks()[0];
     std::vector<double> coordinates;
     nb->get_field_data("mesh_model_coordinates", coordinates);
     int64_t num_nodes   = nb->entity_count();
@@ -182,7 +181,7 @@ namespace {
       std::vector<int64_t> connectivity;
 
       for (size_t j = 0; j < sidesets[i]->block_count(); ++j) {
-        Ioss::SideBlock *    block = sidesets[i]->get_block(j);
+        Ioss::SideBlock     *block = sidesets[i]->get_block(j);
         std::vector<int64_t> side_ids_per_block;
         std::vector<int64_t> connectivity_per_block;
 
@@ -326,7 +325,7 @@ namespace {
         int64_t            side_count       = side_blocks[k]->entity_count();
         const std::string &parent_topo_name = side_blocks[k]->parent_element_topology()->name();
         const std::string &side_block_name  = side_blocks[k]->name();
-        Ioss::SideBlock *  side_block =
+        Ioss::SideBlock   *side_block =
             new Ioss::SideBlock(db_out, side_block_name, topo_name, parent_topo_name, side_count);
 
         sideset_output->add(side_block);
@@ -415,8 +414,8 @@ int main(int argc, char **argv)
   ON_BLOCK_EXIT(MPI_Finalize);
 #endif
 
-  auto cli =
-      session.cli() | Opt(input_filename, "filename")["-F"]["--filename"]("The filename path to ADeDA.e");
+  auto cli = session.cli() |
+             Opt(input_filename, "filename")["-F"]["--filename"]("The filename path to ADeDA.e");
 
   session.cli(cli);
 
