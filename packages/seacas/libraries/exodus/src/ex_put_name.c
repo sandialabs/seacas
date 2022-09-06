e3ac3e12f468e687a0d6394ae6ea6f9952d04621
Greg Sjaardema <gsjaardema@gmail.com>
2022-09-06 17:07:47 -0600

    EXODUS: Fix output of map names

--- packages/seacas/libraries/exodus/src/ex_put_name.c
+++ packages/seacas/libraries/exodus/src/ex_put_name.c
@@ -1,5 +1,5 @@
 /*
- * Copyright(C) 1999-2020 National Technology & Engineering Solutions
+ * Copyright(C) 1999-2020, 2022 National Technology & Engineering Solutions
  * of Sandia, LLC (NTESS).  Under the terms of Contract DE-NA0003525 with
  * NTESS, the U.S. Government retains certain rights in this software.
  *
@@ -82,7 +82,13 @@ int ex_put_name(int exoid, ex_entity_type obj_type, ex_entity_id entity_id, cons
     EX_FUNC_LEAVE(EX_FATAL);
   }
 
-  ent_ndx = ex__id_lkup(exoid, obj_type, entity_id);
+  if (obj_type == EX_NODE_MAP || obj_type == EX_ELEM_MAP || obj_type == EX_FACE_MAP ||
+      obj_type == EX_EDGE_MAP) {
+    ent_ndx = entity_id;
+  }
+  else {
+    ent_ndx = ex__id_lkup(exoid, obj_type, entity_id);
+  }
 
   if (ent_ndx == -EX_LOOKUPFAIL) { /* could not find the element block id */
     snprintf(errmsg, MAX_ERR_LENGTH, "ERROR: %s id %" PRId64 " not found in file id %d",
