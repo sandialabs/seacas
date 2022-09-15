f578e3a5beb9b6c60d2361cc8d7151c68616f18e
Greg Sjaardema <gsjaardema@gmail.com>
2022-09-15 16:58:19 -0600

    Coverity fixes

--- packages/seacas/applications/exodiff/create_file.C
+++ packages/seacas/applications/exodiff/create_file.C
@@ -1,4 +1,4 @@
-// Copyright(C) 1999-2021 National Technology & Engineering Solutions
+// Copyright(C) 1999-2022 National Technology & Engineering Solutions
 // of Sandia, LLC (NTESS).  Under the terms of Contract DE-NA0003525 with
 // NTESS, the U.S. Government retains certain rights in this software.
 //
@@ -316,7 +316,7 @@ namespace {
                             const std::vector<std::string> &var_names2, bool *diff_found)
   {
     std::vector<std::string> x_list; // exclusion list
-    for (auto name : names) {
+    for (auto &name : names) {
       chop_whitespace(name);
       SMART_ASSERT(!name.empty());
       if (name[0] == '!') {
