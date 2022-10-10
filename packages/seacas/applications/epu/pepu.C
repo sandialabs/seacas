423fa964cdfd6e879c5cd0b3a1f1e564ae92b45c
Greg Sjaardema <gsjaardema@gmail.com>
2022-10-10 12:21:12 -0600

    EPU: Fix include ordering - mpi.h first

--- packages/seacas/applications/epu/pepu.C
+++ packages/seacas/applications/epu/pepu.C
@@ -1,5 +1,5 @@
 /*
- * Copyright(C) 1999-2020 National Technology & Engineering Solutions
+ * Copyright(C) 1999-2020, 2022 National Technology & Engineering Solutions
  * of Sandia, LLC (NTESS).  Under the terms of Contract DE-NA0003525 with
  * NTESS, the U.S. Government retains certain rights in this software.
  *
@@ -16,8 +16,9 @@
 #endif
 
 #if ENABLE_PARALLEL_EPU
-#include "epu.C"
 #include <mpi.h>
+
+#include "epu.C"
 #else
 #error "PEPU: Parallel capability not enabled. This should not happen."
 #endif
