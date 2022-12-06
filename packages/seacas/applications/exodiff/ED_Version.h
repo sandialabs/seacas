a3c96879620326ecb97a5fd202b5aab40b1a1bab
Greg Sjaardema <gsjaardema@gmail.com>
2022-12-06 08:18:20 -0700

    EXODIFF: More tolerant file parsing; use pragma once
    The exodiff input file will now recognize lines that start with one or
    more spaces instead of requiring that the variable lines start with
    one or more tabs.
    
    Also added pragma once to all include files.

--- packages/seacas/applications/exodiff/ED_Version.h
+++ packages/seacas/applications/exodiff/ED_Version.h
@@ -3,10 +3,7 @@
 // NTESS, the U.S. Government retains certain rights in this software.
 //
 // See packages/seacas/LICENSE for details
-#ifndef ED_Version_h
-#define ED_Version_h
+#pragma once
 
-static std::string version("3.23");
-static std::string verdate("2022-11-01");
-
-#endif // ED_Version_h
+static std::string version("3.24");
+static std::string verdate("2022-12-06");
