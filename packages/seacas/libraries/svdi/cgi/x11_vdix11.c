68ed48f0da43ebab20f222b1035bb35c4e124398
Greg Sjaardema <gsjaardema@gmail.com>
2023-06-13 13:49:52 -0600

    SVDI: Clean up unused files and unused include files

--- packages/seacas/libraries/svdi/cgi/x11_vdix11.c
+++ packages/seacas/libraries/svdi/cgi/x11_vdix11.c
@@ -424,13 +424,10 @@ void vbstmp(void);
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#include <X11/Xatom.h>
 #include <X11/Xlib.h>
-#include <X11/Xos.h>
-#include <X11/Xresource.h>
 #include <X11/Xutil.h>
-#include <X11/keysym.h>
 
 /* svdi state */
 /* attributes
