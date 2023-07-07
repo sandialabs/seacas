e2e13518a1bf79d833cb70a82ac24e3077d1a4b1
Greg Sjaardema <gsjaardema@gmail.com>
2023-07-07 17:15:48 -0600

    IOSS: Warning fixes from Trilinos snapshot

--- packages/seacas/libraries/ioss/src/Ioss_Region.C
+++ packages/seacas/libraries/ioss/src/Ioss_Region.C
@@ -2824,11 +2824,11 @@ namespace Ioss {
     const Ioss::ElementBlockContainer &elem_blocks = get_element_blocks();
 
     verify_field_exists_on_any_entity_group(field_name, *this, elem_blocks, "input");
+
     size_t field_count = get_all_block_field_data_count(field_name, elem_blocks);
-
     field_data.resize(field_count);
-    size_t data_size = field_count * sizeof(T);
 
+    size_t              data_size = field_count * sizeof(T);
     std::vector<size_t> offsets =
         internal_get_all_block_field_data(field_name, field_data.data(), data_size);
 
