151a382c9ffca95c10eaa591c6b1405e346afd81
Greg Sjaardema <gsjaardema@gmail.com>
2023-07-19 11:20:23 -0600

    EXODUS: Fix closing of files containing groups

--- packages/seacas/libraries/exodus/include/exodusII.h
+++ packages/seacas/libraries/exodus/include/exodusII.h
@@ -52,12 +52,12 @@
 #endif
 
 /* EXODUS version number */
-#define EXODUS_VERSION       "8.23"
+#define EXODUS_VERSION       "8.24"
 #define EXODUS_VERSION_MAJOR 8
-#define EXODUS_VERSION_MINOR 23
-#define EXODUS_RELEASE_DATE  "July 13, 2023"
+#define EXODUS_VERSION_MINOR 24
+#define EXODUS_RELEASE_DATE  "July 19, 2023"
 
-#define EX_API_VERS       8.23f
+#define EX_API_VERS       8.24f
 #define EX_API_VERS_NODOT (100 * EXODUS_VERSION_MAJOR + EXODUS_VERSION_MINOR)
 #define EX_VERS           EX_API_VERS
 
