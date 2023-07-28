057b3cb38dfee982f5eeb4eff7d0e98b24f270d9
Greg Sjaardema <gsjaardema@gmail.com>
2023-07-28 12:47:51 -0600

    NEM_SPREAD: Use free for releasing callocd memory

--- packages/seacas/applications/nem_spread/pe_write_parExo_info.C
+++ packages/seacas/applications/nem_spread/pe_write_parExo_info.C
@@ -514,7 +514,7 @@ void NemSpread<T, INT>::write_parExo_data(int mesh_exoid, int max_name_length, i
 
     // Clear out allocated memory for assembly entity_list...
     for (auto &assembly : globals.Assemblies) {
-      delete[] assembly.name;
+      free assembly.name;
       delete[] assembly.entity_list;
       assembly.name        = nullptr;
       assembly.entity_list = nullptr;
