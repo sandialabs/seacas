5e529ad952841ce624dc00c68924f43d0d036e7b
Greg Sjaardema <gsjaardema@gmail.com>
2023-09-20 17:05:55 -0600

    EXODUS: Clean up anothr bad merge

--- packages/seacas/libraries/exodus/src/ex_utils.c
+++ packages/seacas/libraries/exodus/src/ex_utils.c
@@ -2380,94 +2380,95 @@ int ex__get_varid(int exoid, ex_entity_type obj_type, ex_entity_id id)
         return EX_FATAL;
       }
     }
-    return varid;
   }
+  return varid;
+}
 
-  /*
-   * Code from:
-   * https://stackoverflow.com/questions/11034002/how-to-get-absolute-path-of-file-or-directory-that-does-not-exist
-   *
-   * Return the input path in a canonical form. This is achieved by
-   * expanding all symbolic links, resolving references to "." and "..",
-   * and removing duplicate "/" characters.
-   *
-   * If the file exists, its path is canonicalized and returned. If the file,
-   * or parts of the containing directory, do not exist, path components are
-   * removed from the end until an existing path is found. The remainder of the
-   * path is then appended to the canonical form of the existing path,
-   * and returned. Consequently, the returned path may not exist. The portion
-   * of the path which exists, however, is represented in canonical form.
-   *
-   * If successful, this function returns a C-string, which needs to be freed by
-   * the caller using free().
-   *
-   * ARGUMENTS:
-   *   file_path
-   *   File path, whose canonical form to return.
-   *
-   * RETURNS:
-   *   On success, returns the canonical path to the file, which needs to be freed
-   *   by the caller.
-   *
-   *   On failure, returns NULL.
-   */
-  char *ex__canonicalize_filename(char const *file_path)
-  {
+/*
+ * Code from:
+ * https://stackoverflow.com/questions/11034002/how-to-get-absolute-path-of-file-or-directory-that-does-not-exist
+ *
+ * Return the input path in a canonical form. This is achieved by
+ * expanding all symbolic links, resolving references to "." and "..",
+ * and removing duplicate "/" characters.
+ *
+ * If the file exists, its path is canonicalized and returned. If the file,
+ * or parts of the containing directory, do not exist, path components are
+ * removed from the end until an existing path is found. The remainder of the
+ * path is then appended to the canonical form of the existing path,
+ * and returned. Consequently, the returned path may not exist. The portion
+ * of the path which exists, however, is represented in canonical form.
+ *
+ * If successful, this function returns a C-string, which needs to be freed by
+ * the caller using free().
+ *
+ * ARGUMENTS:
+ *   file_path
+ *   File path, whose canonical form to return.
+ *
+ * RETURNS:
+ *   On success, returns the canonical path to the file, which needs to be freed
+ *   by the caller.
+ *
+ *   On failure, returns NULL.
+ */
+char *ex__canonicalize_filename(char const *file_path)
+{
 #if defined(WIN32) || defined(__WIN32__) || defined(_WIN32) || defined(_MSC_VER) ||                \
     defined(__MINGW32__) || defined(_WIN64) || defined(__MINGW64__)
-    return _fullpath(NULL, file_path, _MAX_PATH);
+  return _fullpath(NULL, file_path, _MAX_PATH);
 #else
-    char        *canonical_file_path = NULL;
-    unsigned int file_path_len       = strlen(file_path);
+  char        *canonical_file_path = NULL;
+  unsigned int file_path_len       = strlen(file_path);
 
-    if (file_path_len > 0) {
-      canonical_file_path = realpath(file_path, NULL);
-      if (canonical_file_path == NULL && errno == ENOENT) {
-        // The file was not found. Back up to a segment which exists,
-        // and append the remainder of the path to it.
-        char *file_path_copy = NULL;
-        if (file_path[0] == '/' || (strncmp(file_path, "./", 2) == 0) ||
-            (strncmp(file_path, "../", 3) == 0)) {
-          // Absolute path, or path starts with "./" or "../"
-          file_path_copy = strdup(file_path);
-        }
-        else {
-          // Relative path
-          file_path_copy = (char *)malloc(strlen(file_path) + 3);
-          strcpy(file_path_copy, "./");
-          strcat(file_path_copy, file_path);
-        }
+  if (file_path_len > 0) {
+    canonical_file_path = realpath(file_path, NULL);
+    if (canonical_file_path == NULL && errno == ENOENT) {
+      // The file was not found. Back up to a segment which exists,
+      // and append the remainder of the path to it.
+      char *file_path_copy = NULL;
+      if (file_path[0] == '/' || (strncmp(file_path, "./", 2) == 0) ||
+          (strncmp(file_path, "../", 3) == 0)) {
+        // Absolute path, or path starts with "./" or "../"
+        file_path_copy = strdup(file_path);
+      }
+      else {
+        // Relative path
+        file_path_copy = (char *)malloc(strlen(file_path) + 3);
+        strcpy(file_path_copy, "./");
+        strcat(file_path_copy, file_path);
+      }
 
-        // Remove path components from the end, until an existing path is found
-        for (int char_idx = strlen(file_path_copy) - 1;
-             char_idx >= 0 && canonical_file_path == NULL; --char_idx) {
-          if (file_path_copy[char_idx] == '/') {
-            // Remove the slash character
-            file_path_copy[char_idx] = '\0';
+      // Remove path components from the end, until an existing path is found
+      for (int char_idx = strlen(file_path_copy) - 1; char_idx >= 0 && canonical_file_path == NULL;
+           --char_idx) {
+        if (file_path_copy[char_idx] == '/') {
+          // Remove the slash character
+          file_path_copy[char_idx] = '\0';
 
-            canonical_file_path = realpath(file_path_copy, NULL);
-            if (canonical_file_path != NULL) {
-              // An existing path was found. Append the remainder of the path
-              // to a canonical form of the existing path.
-              char *combined_file_path = (char *)malloc(strlen(canonical_file_path) +
-                                                        strlen(file_path_copy + char_idx + 1) + 2);
-              strcpy(combined_file_path, canonical_file_path);
-              strcat(combined_file_path, "/");
-              strcat(combined_file_path, file_path_copy + char_idx + 1);
-              free(canonical_file_path);
-              canonical_file_path = combined_file_path;
-            }
-            else {
-              // The path segment does not exist. Replace the slash character
-              // and keep trying by removing the previous path component.
-              file_path_copy[char_idx] = '/';
-            }
+          canonical_file_path = realpath(file_path_copy, NULL);
+          if (canonical_file_path != NULL) {
+            // An existing path was found. Append the remainder of the path
+            // to a canonical form of the existing path.
+            char *combined_file_path = (char *)malloc(strlen(canonical_file_path) +
+                                                      strlen(file_path_copy + char_idx + 1) + 2);
+            strcpy(combined_file_path, canonical_file_path);
+            strcat(combined_file_path, "/");
+            strcat(combined_file_path, file_path_copy + char_idx + 1);
+            free(canonical_file_path);
+            canonical_file_path = combined_file_path;
+          }
+          else {
+            // The path segment does not exist. Replace the slash character
+            // and keep trying by removing the previous path component.
+            file_path_copy[char_idx] = '/';
           }
         }
-
-        free(file_path_copy);
       }
+
+      free(file_path_copy);
     }
-    return canonical_file_path;
-#endif
   }
+  return canonical_file_path;
+#endif
+}
