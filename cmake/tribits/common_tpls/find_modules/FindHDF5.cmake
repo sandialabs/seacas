4850a339c80b71c2aeffd6fca7d4f50ce2f5848d
Greg Sjaardema <gsjaardema@gmail.com>
2023-10-13 08:19:24 -0600

    Update FindHDF5.cmake
    Fix another typo

--- cmake/tribits/common_tpls/find_modules/FindHDF5.cmake
+++ cmake/tribits/common_tpls/find_modules/FindHDF5.cmake
@@ -389,8 +389,8 @@ else()
       set(HDF5_INCLUDE_DIRS ${HDF5_INCLUDE_DIR})
 
       # Define HDF5_C_LIBRARIES to contain hdf5 and hdf5_hl C libraries
-      if( HDF5_HL_LIBRARY AND HDF5_CLIBRARY)
-        set(HDF5_LIBRARIES ${HDF5_HL_LIBRARY} ${HDF5_CLIBRARY})
+      if( HDF5_HL_LIBRARY AND HDF5_C_LIBRARY)
+        set(HDF5_LIBRARIES ${HDF5_HL_LIBRARY} ${HDF5_C_LIBRARY})
       else()
         set(HDF5_LIBRARIES ${HDF5_HL_STATIC_LIBRARY} ${HDF5_C_STATIC_LIBRARY})
       endif()
