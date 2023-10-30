df03dfce6ed7f1a777f9c700de9146391b3081e7
Greg Sjaardema <gsjaardema@gmail.com>
2023-10-30 13:56:03 -0600

    EXODUS: Add missing functions for mingw

--- packages/seacas/libraries/exodus/src/ex_field_utils.c
+++ packages/seacas/libraries/exodus/src/ex_field_utils.c
@@ -14,6 +14,38 @@
 
 #define SIZE(X) sizeof(X) / sizeof(X[0])
 
+#if defined(__MINGW32__) || defined(_WIN64) || defined(__MINGW64__)
+char *strsep(char **stringp, const char *delim)
+{
+  char *rv = *stringp;
+  if (rv) {
+    *stringp += strcspn(*stringp, delim);
+    if (**stringp)
+      *(*stringp)++ = '\0';
+    else
+      *stringp = 0;
+  }
+  return rv;
+}
+
+size_t strlcat(char *restrict dst, const char *restrict src, size_t maxlen)
+{
+  const size_t srclen = strlen(src);
+  const size_t dstlen = strnlen(dst, maxlen);
+  if (dstlen == maxlen)
+    return maxlen + srclen;
+  if (srclen < maxlen - dstlen) {
+    memcpy(dst + dstlen, src, srclen + 1);
+  }
+  else {
+    memcpy(dst + dstlen, src, maxlen - 1);
+    dst[dstlen + maxlen - 1] = '\0';
+  }
+  return dstlen + srclen;
+}
+
+#endif
+
 static int number_width(const size_t number)
 {
   if (number == 0) {
