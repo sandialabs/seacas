63aab831ce66d988f89830e890eee6111f536e08
Greg Sjaardema <gsjaardema@gmail.com>
2024-02-14 10:40:29 -0700

    IOSS: More conversion .data() to Data(..) -- exodus

--- packages/seacas/libraries/ioss/src/exodus/Ioex_Utils.C
+++ packages/seacas/libraries/ioss/src/exodus/Ioex_Utils.C
@@ -1,4 +1,4 @@
-// Copyright(C) 1999-2023 National Technology & Engineering Solutions
+// Copyright(C) 1999-2024 National Technology & Engineering Solutions
 // of Sandia, LLC (NTESS).  Under the terms of Contract DE-NA0003525 with
 // NTESS, the U.S. Government retains certain rights in this software.
 //
@@ -774,11 +774,11 @@ namespace Ioex {
         break;
       case Ioss::Property::BasicType::VEC_INTEGER:
         ex_put_integer_attribute(exoid, type, id, property_name.c_str(), prop.get_vec_int().size(),
-                                 prop.get_vec_int().data());
+                                 Data(prop.get_vec_int()));
         break;
       case Ioss::Property::BasicType::VEC_DOUBLE:
         ex_put_double_attribute(exoid, type, id, property_name.c_str(),
-                                prop.get_vec_double().size(), prop.get_vec_double().data());
+                                prop.get_vec_double().size(), Data(prop.get_vec_double()));
         break;
       default:; // Do nothing
       }
