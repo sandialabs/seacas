d7b3b576cc4002629ded9289fd481b82fe5d2cd7
Greg Sjaardema <gdsjaar@sandia.gov>
2024-06-04 21:37:30 -0600

    IOSS: Explicit template instantiation fix

--- packages/seacas/libraries/ioss/src/Ioss_FaceGenerator.C
+++ packages/seacas/libraries/ioss/src/Ioss_FaceGenerator.C
@@ -1,4 +1,4 @@
-// Copyright(C) 1999-2023 National Technology & Engineering Solutions
+// Copyright(C) 1999-2024 National Technology & Engineering Solutions
 // of Sandia, LLC (NTESS).  Under the terms of Contract DE-NA0003525 with
 // NTESS, the U.S. Government retains certain rights in this software.
 //
@@ -355,6 +355,9 @@ namespace Ioss {
       hashIds_.push_back(id_hash(id));
     }
   }
+
+  template IOSS_EXPORT void FaceGenerator::generate_block_faces(const Ioss::ElementBlockContainer &, int, bool);
+  template IOSS_EXPORT void FaceGenerator::generate_block_faces(const Ioss::ElementBlockContainer &, int64_t, bool);
 
   template <typename INT> void FaceGenerator::generate_block_faces(const Ioss::ElementBlockContainer &ebs, 
 								   INT /*dummy*/, bool local_ids)
