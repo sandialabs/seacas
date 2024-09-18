f6e3e26690ad56847c92b6427220a764f67fb792
tokusanya <tookusa@sandia.gov>
2024-09-17 19:07:02 -0600

    IOSS: Update TextMesh to handle extended shell ordinals to the edges (#495)
    * IOSS: Update TextMesh to handle extended shell ordinals to the edges
    
    * Fix to make sure adjacency graph is limited to faces

--- packages/seacas/libraries/ioss/src/text_mesh/Iotm_TextMeshTopologyMapping.h
+++ packages/seacas/libraries/ioss/src/text_mesh/Iotm_TextMeshTopologyMapping.h
@@ -80,7 +80,7 @@ namespace Iotm {
 
     bool operator!=(const TopologyMapEntry &rhs) const { return !(*this == rhs); }
 
-    int num_sides() const
+    int num_face_sides() const
     {
       if (topology->is_shell()) {
         // Only interested in face boundaries, not edges
@@ -90,6 +90,11 @@ namespace Iotm {
       }
 
       return topology->number_boundaries();
+    }
+
+    int num_sides() const
+    {
+      return sideTopologies.size();
     }
 
     // Side references are one-based
@@ -583,7 +588,7 @@ namespace Iotm {
 
       if (!entry.initialized) {
         entry.set_valid_spatial_dimensions({false, false, false, true});
-        entry.set_side_topologies({tri_3_factory(), tri_3_factory()});
+        entry.set_side_topologies({tri_3_factory(), tri_3_factory(), line_2_factory(), line_2_factory(), line_2_factory()});
         entry.initialized = true;
       }
 
@@ -596,7 +601,7 @@ namespace Iotm {
 
       if (!entry.initialized) {
         entry.set_valid_spatial_dimensions({false, false, false, true});
-        entry.set_side_topologies({tri_4_factory(), tri_4_factory()});
+        entry.set_side_topologies({tri_4_factory(), tri_4_factory(), line_2_factory(), line_2_factory(), line_2_factory()});
         entry.initialized = true;
       }
 
@@ -609,7 +614,7 @@ namespace Iotm {
 
       if (!entry.initialized) {
         entry.set_valid_spatial_dimensions({false, false, false, true});
-        entry.set_side_topologies({tri_6_factory(), tri_6_factory()});
+        entry.set_side_topologies({tri_6_factory(), tri_6_factory(), line_3_factory(), line_3_factory(), line_3_factory()});
         entry.initialized = true;
       }
 
@@ -627,7 +632,7 @@ namespace Iotm {
 
       if (!entry.initialized) {
         entry.set_valid_spatial_dimensions({false, false, false, true});
-        entry.set_side_topologies({quad_4_factory(), quad_4_factory()});
+        entry.set_side_topologies({quad_4_factory(), quad_4_factory(), line_2_factory(), line_2_factory(), line_2_factory(), line_2_factory()});
         entry.initialized = true;
       }
 
@@ -640,7 +645,7 @@ namespace Iotm {
 
       if (!entry.initialized) {
         entry.set_valid_spatial_dimensions({false, false, false, true});
-        entry.set_side_topologies({quad_8_factory(), quad_8_factory()});
+        entry.set_side_topologies({quad_8_factory(), quad_8_factory(), line_3_factory(), line_3_factory(), line_3_factory(), line_3_factory()});
         entry.initialized = true;
       }
 
@@ -653,7 +658,7 @@ namespace Iotm {
 
       if (!entry.initialized) {
         entry.set_valid_spatial_dimensions({false, false, false, true});
-        entry.set_side_topologies({quad_9_factory(), quad_9_factory()});
+        entry.set_side_topologies({quad_9_factory(), quad_9_factory(), line_3_factory(), line_3_factory(), line_3_factory(), line_3_factory()});
         entry.initialized = true;
       }
 
