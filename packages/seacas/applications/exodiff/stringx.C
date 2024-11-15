b4bbcc347d10d8f391afec74cbc67deff54e268c
Greg Sjaardema <gdsjaar@sandia.gov>
2024-11-14 17:59:36 -0700

    EXODIFF: Changes related to namelist

--- packages/seacas/applications/exodiff/stringx.C
+++ packages/seacas/applications/exodiff/stringx.C
@@ -1,4 +1,4 @@
-// Copyright(C) 1999-2021, 2023 National Technology & Engineering Solutions
+// Copyright(C) 1999-2021, 2023, 2024 National Technology & Engineering Solutions
 // of Sandia, LLC (NTESS).  Under the terms of Contract DE-NA0003525 with
 // NTESS, the U.S. Government retains certain rights in this software.
 //
@@ -6,6 +6,7 @@
 
 #include "smart_assert.h" // for SMART_ASSERT
 #include "stringx.h"
+
 #include <cctype>  // for tolower, isspace
 #include <cstring> // for strspn, strcspn
 #include <string>  // for string, operator==
