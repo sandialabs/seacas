a8cce098237b8300252cc45c1a6ecbefc7dce37e
Greg Sjaardema <gsjaardema@gmail.com>
2025-04-02 12:32:21 -0600

    IOSS: Clean up owning_processor calculation

--- packages/seacas/libraries/ioss/src/exodus/Ioex_ParallelDatabaseIO.C
+++ packages/seacas/libraries/ioss/src/exodus/Ioex_ParallelDatabaseIO.C
@@ -2052,9 +2052,7 @@ namespace Ioex {
           for (size_t i = 0; i < ent_proc.size(); i += 2) {
             int64_t node = ent_proc[i + 0];
             int64_t proc = ent_proc[i + 1];
-            if (proc < idata[node - 1]) {
-              idata[node - 1] = proc;
-            }
+	    idata[node - 1] = std::min(idata[node - 1], static_cast<int>(proc));
           }
         }
         else {
@@ -2066,9 +2064,7 @@ namespace Ioex {
           for (size_t i = 0; i < ent_proc.size(); i += 2) {
             int node = ent_proc[i + 0];
             int proc = ent_proc[i + 1];
-            if (proc < idata[node - 1]) {
-              idata[node - 1] = proc;
-            }
+	    idata[node - 1] = std::min(idata[node - 1], proc);
           }
         }
       }
