13e335548665a1d1c54e545d2cb6534b4ad594fb
Greg Sjaardema <gsjaardema@gmail.com>
2025-09-11 16:00:29 -0600

    IOSS: Fix bad use of fmt

--- packages/seacas/libraries/ioss/src/Ioss_FileInfo.C
+++ packages/seacas/libraries/ioss/src/Ioss_FileInfo.C
@@ -398,15 +398,15 @@ namespace Ioss {
         const int mode = 0777; // Users umask will be applied to this.
         if (mkdir(path_root.c_str(), mode) != 0 && errno != EEXIST) {
 #endif
-          fmt::print(errmsg, "ERROR: Cannot create directory '{}': {}\n", path_root,
-                     std::strerror(errno));
+	  errmsg = fmt::format("ERROR: Cannot create directory '{}': {}\n", 
+		     path_root, std::strerror(errno));
           error_found = true;
           break;
         }
       }
       else if (!S_ISDIR(st.st_mode)) {
         errno = ENOTDIR;
-        fmt::print(errmsg, "ERROR: Path '{}' is not a directory.\n", path_root);
+	errmsg = fmt::format("ERROR: Path '{}' is not a directory.\n", path_root);
         error_found = true;
         break;
       }
