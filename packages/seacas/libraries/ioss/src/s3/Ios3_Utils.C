70f6d20a5536f22b29212e92142668b27cdc2e2d
Todd Kordenbrock <thkorde@sandia.gov>
2025-09-16 09:09:47 -0500

    update copyright header

--- packages/seacas/libraries/ioss/src/s3/Ios3_Utils.C
+++ packages/seacas/libraries/ioss/src/s3/Ios3_Utils.C
@@ -1,5 +1,5 @@
-// Copyright(C) 1999-2022 National Technology & Engineering Solutions
-// of Sandia, LLC (NTESS).  Under the terms of Contract DE-NA0003525 with
+// Copyright(C) 2025 National Technology & Engineering Solutions
+// of Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525 with
 // NTESS, the U.S. Government retains certain rights in this software.
 //
 // See packages/seacas/LICENSE for details
