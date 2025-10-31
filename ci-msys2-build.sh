8f400d796b626dbcb413aca3962f1eb56a016777
Greg Sjaardema <gsjaardema@gmail.com>
2025-10-31 14:25:59 -0600

    CI: Fix HAVE_X11 --> X11 symbol change

--- ci-msys2-build.sh
+++ ci-msys2-build.sh
@@ -6,7 +6,7 @@ echo $HOME
 mkdir build
 cd build
 
-HAVE_X11=NO NETCDF_PATH=/mingw64 HDF5_PATH=/mingw64 CGNS_PATH=/mingw64 MPI=NO bash ../cmake-config
+X11=NO NETCDF_PATH=/mingw64 HDF5_PATH=/mingw64 CGNS_PATH=/mingw64 MPI=NO bash ../cmake-config
 make -k -j4
 make install
 #ctest -j 4 --output-on-failure
